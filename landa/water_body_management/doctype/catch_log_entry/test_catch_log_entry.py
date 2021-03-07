5ba2252f15bf017287379fa82da69980229e7e2f
barredterra <14891507+barredterra@users.noreply.github.com>
2021-03-07 16:46:25 +0100

    refactor: Modules "Organization Management" and "Water Body Management"

---
+++ landa/water_body_management/doctype/catch_log_entry/test_catch_log_entry.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2021, Landesverband Sächsischer Angler e. V.Real Experts GmbH and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+# import frappe
+import unittest
+
+class TestCatchLogEntry(unittest.TestCase):
+	pass
---
+++ landa/water_body_management/doctype/fishing_area/fishing_area.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Landesverband Sächsischer Angler e. V.Real Experts GmbH and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Fishing Area', {
+	// refresh: function(frm) {
+
+	// }
+});
