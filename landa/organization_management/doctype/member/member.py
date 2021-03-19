91e25dcf0d3915dbad8bcf3ef5cc7da07ac39abf
barredterra <14891507+barredterra@users.noreply.github.com>
2021-03-19 17:15:49 +0100

    feat: autoname for members

--- landa/organization_management/doctype/member/member.py
+++ landa/organization_management/doctype/member/member.py
@@ -5,12 +5,28 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from frappe.model.naming import make_autoname
+from frappe.model.naming import revert_series_if_last
 
 class Member(Document):
-	
+
+	def autoname(self):
+		"""Generate the unique member number (name field)
+
+		Organization:				AVL-001-0001, AVE-001-0001, ...
+		Local group:				AVL-001-01-0001, AVL-001-02-0001, ...
+		"""
+		if self.name:
+			return
+
+		self.name = make_autoname(self.organization + '-.####', 'Member')
+
 	def on_update(self):
 		if self.user and self.create_user_permission:
 			self.create_user_permissions()
+
+	def on_trash(self):
+		self.revert_series()
 
 	def create_user_permissions(self):
 		"""Restrict Member to itself and it's Organization."""
@@ -30,3 +46,11 @@ class Member(Document):
 			)).insert()
 		except frappe.DuplicateEntryError:
 			frappe.clear_messages()
+
+	def revert_series(self):
+		"""Decrease the naming counter when the newest member gets deleted."""
+		# reconstruct the key used to generate the name
+		number_part_len = len(self.name.split('-')[-1])
+		key = self.name[:-number_part_len] + '.' + '#' * number_part_len
+
+		revert_series_if_last(key, self.name)
