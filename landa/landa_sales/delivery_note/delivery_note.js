86e6e7d22aea134df62e6e4f0902ca06b9d83aca
Raffael Meyer <14891507+barredterra@users.noreply.github.com>
2021-04-23 17:19:22 +0200

    fix: use item query
    LANDA-105
    
    Co-authored-by: Samuel Danieli <23150094+scdanieli@users.noreply.github.com>

--- landa/landa_sales/delivery_note/delivery_note.js
+++ landa/landa_sales/delivery_note/delivery_note.js
@@ -4,6 +4,7 @@ frappe.ui.form.on('Delivery Note',  {
             // some items cannot be returned
             frm.set_query("item_code", "items", function () {
                 return {
+                    query: "erpnext.controllers.queries.item_query",
                     filters: {
                         'cannot_be_returned': 0,
                         'is_sales_item': 1
