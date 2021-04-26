f49c75e946883bf8a286dbba151c01127b4a482b
barredterra <14891507+barredterra@users.noreply.github.com>
2021-04-26 16:48:46 +0200

    feat: customer ledger (LANDA-113)

---
+++ landa/landa_sales/report/landa_customer_ledger/landa_customer_ledger.js
@@ -0,0 +1,32 @@
+// Copyright (c) 2016, Landesverband Sächsischer Angler e. V.Real Experts GmbH and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["LANDA Customer Ledger"] = {
+	"filters": [
+		{
+			"fieldname": "company",
+			"fieldtype": "Link",
+			"label": "Company",
+			"mandatory": 1,
+			"options": "Company",
+			"wildcard_filter": 0
+		},
+		{
+			"fieldname": "customer",
+			"fieldtype": "Link",
+			"label": "Customer",
+			"mandatory": 1,
+			"options": "Customer",
+			"wildcard_filter": 0
+		},
+		{
+			"fieldname": "fiscal_year",
+			"fieldtype": "Link",
+			"label": "Fiscal Year",
+			"mandatory": 0,
+			"options": "Fiscal Year",
+			"wildcard_filter": 0
+		}
+	]
+};
