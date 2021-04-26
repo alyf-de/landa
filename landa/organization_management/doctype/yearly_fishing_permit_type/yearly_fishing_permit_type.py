27d1227e405f051db8509d0c20bd6c0bb967dafb
Samuel Danieli <23150094+scdanieli@users.noreply.github.com>
2021-04-26 18:48:51 +0200

    feat: add attribute value for permit type (LANDA-95)

--- landa/organization_management/doctype/yearly_fishing_permit_type/yearly_fishing_permit_type.py
+++ landa/organization_management/doctype/yearly_fishing_permit_type/yearly_fishing_permit_type.py
@@ -3,8 +3,30 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class YearlyFishingPermitType(Document):
-	pass
+	def on_submit(self):
+		add_attribute_value(
+			"Erlaubnisscheinart",
+			self.yearly_fishing_permit_type_name,
+			self.short_code
+		)
+
+def add_attribute_value(name, attribute_value, abbr):
+	create_item_attribute(name)
+	item_attr = frappe.get_doc("Item Attribute", name)
+	item_attr.append('item_attribute_values', {
+		'attribute_value': attribute_value,
+		'abbr': abbr
+	})
+	item_attr.save()
+
+
+def create_item_attribute(name):
+	"""Create an Item Attribute if there isn't one already"""
+	if not frappe.db.exists("Item Attribute", name):
+		item_attr = frappe.new_doc("Item Attribute")
+		item_attr.attribute_name = name
+		item_attr.insert()
