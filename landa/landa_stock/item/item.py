19fb510b52029cc599de70ebec9634104e7ab62b
Samuel Danieli <23150094+scdanieli@users.noreply.github.com>
2021-05-17 13:40:48 +0200

    feat: simplify item quick entry, add tax template (LANDA-163, LANDA-177)

--- landa/landa_stock/item/item.py
+++ landa/landa_stock/item/item.py
@@ -1,5 +1,10 @@
 
 def before_insert(item, event):
+	set_year_of_validity(item)
+	set_tax_template(item)
+
+
+def set_year_of_validity(item):
 	"""Set "Valid From Year" and "Valid To Year" to year_of_validity from Attribute Value."""
 	if item.variant_of and item.attributes:
 		years = [row.attribute_value for row in item.attributes if row.attribute == 'Gültigkeitsjahr']
@@ -7,3 +12,10 @@ def before_insert(item, event):
 			year = years[0]
 			item.valid_from_year = year
 			item.valid_to_year = year
+	
+
+def set_tax_template(item):
+	if item.item_tax_template:
+		item.append('taxes', {
+			'item_tax_template': item.item_tax_template,
+		})
