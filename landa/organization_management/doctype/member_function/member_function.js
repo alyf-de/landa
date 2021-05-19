9945549b08b1bcd4c58c98fc14d89b96d27c9d5d
barredterra <14891507+barredterra@users.noreply.github.com>
2021-05-19 19:13:56 +0200

    feat: Local Organization can assign only local Member Function Category (LANDA-124)

--- landa/organization_management/doctype/member_function/member_function.js
+++ landa/organization_management/doctype/member_function/member_function.js
@@ -2,7 +2,21 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Member Function', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		frm.trigger('set_member_function_category_query');
+	},
+	set_member_function_category_query: function(frm) {
+		if (
+			frappe.user.has_role('LANDA Local Organization Management') &&
+			!frappe.user.has_role('LANDA Regional Organization Management')
+		) {
+			frm.set_query('member_function_category', () => {
+				return {
+					filters: {
+						access_level: 'Local Organization'
+					}
+				};
+			});
+		}
+	}
 });
