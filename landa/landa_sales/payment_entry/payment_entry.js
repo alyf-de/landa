20f6507c39c35ce89915382a5eb8c0566f8de593
barredterra <14891507+barredterra@users.noreply.github.com>
2021-05-21 18:42:12 +0200

    style: add comments (LANDA-143)

--- landa/landa_sales/payment_entry/payment_entry.js
+++ landa/landa_sales/payment_entry/payment_entry.js
@@ -3,13 +3,17 @@ frappe.ui.form.on('Payment Entry', {
         frm.trigger('set_year_of_settlement');
     },
     set_year_of_settlement(frm) {
+        // Do nothing if year_of_settlement is already set
         if (!frm.doc.year_of_settlement){
+            // Get all rows where a Sales Invoice or Sales Order is linked
             const references = frm.doc.references.filter(
                 (ref) =>
                     ['Sales Invoice', 'Sales Order'].includes(ref.reference_doctype) &&
                     ref.reference_name
             );
             if (references) {
+                // we can only have one year of settlement, therefore we just
+                // take it from the first reference
                 const ref = references[0];
                 frappe.db.get_value(ref.reference_doctype, ref.reference_name, 'year_of_settlement', (message) => {
                     if (message && message.year_of_settlement) {
