17d94aca02f484a0a0981ae2403b2eba8c1a5c43
Samuel Danieli <23150094+scdanieli@users.noreply.github.com>
2021-06-08 00:02:31 +0200

    fix: return ... and hide

--- landa/landa_sales/sales_order/sales_order.py
+++ landa/landa_sales/sales_order/sales_order.py
@@ -7,3 +7,6 @@ from frappe.utils.data import get_year_ending
 def before_validate(delivery_note, event):
 	for item in delivery_note.items:
 		item.delivery_date = get_year_ending(str(delivery_note.year_of_settlement))
+
+	if not delivery_note.tax_category:
+		delivery_note.tax_category = "Umsatzsteuer"
