472120b10603ead46ba551eb20f3273f1b399662
barredterra <14891507+barredterra@users.noreply.github.com>
2021-06-08 12:12:07 +0200

    feat: add Customer to session default settings

--- landa/hooks.py
+++ landa/hooks.py
@@ -56,7 +56,7 @@ landa_create_after_install = [
 # Used in `landa.install.disable_modes_of_payment`
 disable_modes_of_payment = ["Wire Transfer", "Cash", "Bank Draft", "Credit Card", "Cheque"]
 
-landa_add_to_session_defaults = "Organization"
+landa_add_to_session_defaults = ["Organization", "Customer"]
 
 on_session_creation = "landa.overrides.set_user_defaults"
 
