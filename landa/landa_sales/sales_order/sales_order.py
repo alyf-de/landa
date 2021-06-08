b51346cbdc580ba1504ac1fa8cd0183581a1996b
barredterra <14891507+barredterra@users.noreply.github.com>
2021-06-08 12:30:18 +0200

    feat: check if tax category exists before trying to set it

--- landa/landa_sales/sales_order/sales_order.py
+++ landa/landa_sales/sales_order/sales_order.py
@@ -2,11 +2,12 @@
 # Copyright (c) 2021, Real Experts GmbH and contributors
 # For license information, please see license.txt
 
+import frappe
 from frappe.utils.data import get_year_ending
 
 def before_validate(sales_order, event):
 	for item in sales_order.items:
 		item.delivery_date = get_year_ending(str(sales_order.year_of_settlement))
 
-	if not sales_order.tax_category:
+	if (not sales_order.tax_category) and frappe.db.exists("Tax Category", "Umsatzsteuer"):
 		sales_order.tax_category = "Umsatzsteuer"
