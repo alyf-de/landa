18d4b84c46af6ad99544247aaa36fd1767ecdddf
barredterra <14891507+barredterra@users.noreply.github.com>
2021-09-24 15:56:56 +0200

    fix: item hooks

--- landa/hooks.py
+++ landa/hooks.py
@@ -154,7 +154,8 @@ doc_events = {
 		"validate": "landa.landa_stock.delivery_note.delivery_note.validate"
 	},
 	"Item": {
-		"before_insert": "landa.landa_stock.item.item.before_insert"
+		"before_insert": "landa.landa_stock.item.item.before_insert",
+		"autoname": "landa.landa_stock.item.item.autoname"
 	},
 	"Sales Order": {
 		"before_validate": "landa.landa_sales.sales_order.sales_order.before_validate"
@@ -162,9 +163,6 @@ doc_events = {
 	"Payment Entry": {
 		"before_validate": "landa.landa_sales.payment_entry.pament_entry.before_validate"
 	},
-	"Item": {
-		"autoname": "landa.landa_stock.item.item.autoname"
-	}
 }
 
 # Scheduled Tasks
