2d3f32beb45c822051a9aacbc7ba818b7d45da33
barredterra <14891507+barredterra@users.noreply.github.com>
2021-10-13 12:12:04 +0200

    refactor: formatting

--- landa/address_and_contact.py
+++ landa/address_and_contact.py
@@ -3,13 +3,17 @@ from frappe import _
 
 
 def has_permission(doc, ptype, user):
-	if not doc.links and not frappe.db.get_single_value("System Settings", "apply_strict_user_permissions"):
+	if not doc.links and not frappe.db.get_single_value(
+		"System Settings", "apply_strict_user_permissions"
+	):
 		# No links and strict user permissions disabled
 		return True
 
 	for link in doc.links:
 		linked_doc = frappe.get_doc(link.link_doctype, link.link_name)
-		if frappe.has_permission(link.link_doctype, ptype=ptype, doc=linked_doc, user=user):
+		if frappe.has_permission(
+			link.link_doctype, ptype=ptype, doc=linked_doc, user=user
+		):
 			# We have permission on one linked doc
 			return True
 
