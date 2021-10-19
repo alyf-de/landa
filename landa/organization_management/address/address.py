1c9872d9f8bb5369e71db1a1525af93a1a56b58e
barredterra <14891507+barredterra@users.noreply.github.com>
2021-10-19 15:08:28 +0200

    feat: Prefer the member's name as address title

--- landa/organization_management/address/address.py
+++ landa/organization_management/address/address.py
@@ -1,4 +1,3 @@
-
 import frappe
 
 from frappe import _
@@ -13,27 +12,40 @@ def autoname(address, event):
 	"""
 	if address.links:
 		for link in address.links:
+			if link.link_doctype == "LANDA Member":
+				member_names = frappe.get_value(
+					"LANDA Member", link.link_name, ["first_name", "last_name"]
+				)
+				full_name = " ".join(member_names)
+				link.link_title = full_name
+
 			if link.link_doctype == "Organization":
-				organization_name = frappe.get_value("Organization", link.link_name, "organization_name")
-				address.address_title = organization_name
+				organization_name = frappe.get_value(
+					"Organization", link.link_name, "organization_name"
+				)
 				link.link_title = organization_name
 
 			if link.link_doctype == "Customer":
-				customer_name = frappe.get_value("Customer", link.link_name, "customer_name")
-				address.address_title = customer_name
+				customer_name = frappe.get_value(
+					"Customer", link.link_name, "customer_name"
+				)
 				link.link_title = customer_name
 
+		# Prefer the member's name as address title otherwise, use any link title as address title
+		for link in address.links:
+			address.address_title = link.link_title
 			if link.link_doctype == "LANDA Member":
-				member_names = frappe.get_value("LANDA Member", link.link_name, ["first_name", "last_name"])
-				full_name = " ".join(member_names)
-				address.address_title = full_name
-				link.link_title = full_name
+				break
 
 		if not address.address_title:
 			address.address_title = address.links[0].link_title
 
 	if address.address_title:
-		address.name = (cstr(address.address_title).strip() + " - " + cstr(_(address.address_type)).strip())
+		address.name = (
+			cstr(address.address_title).strip()
+			+ " - "
+			+ cstr(_(address.address_type)).strip()
+		)
 		if frappe.db.exists("Address", address.name):
 			address.name = make_autoname(address.name + "-.#")
 	else:
