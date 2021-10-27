9156f46a3aba1fa23234fad79cceab0e46bafca3
melanieroedel <roedel@realexperts.de>
2021-10-27 17:02:34 +0200

    feat: initializate report combining all member infos LANDA-299 LANDA-300

---
+++ landa/organization_management/report/current_member_data_report/current_member_data_report.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2013, Real Experts GmbH and contributors
+# For license information, please see license.txt
+
+# import frappe
+
+def execute(filters=None):
+	columns, data = [], []
+	return columns, data
