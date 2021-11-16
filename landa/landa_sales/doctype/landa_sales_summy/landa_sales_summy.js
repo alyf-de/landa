0175e1462d64493290c7aa56da7b0291520c4c0b
barredterra <14891507+barredterra@users.noreply.github.com>
2021-11-16 19:14:48 +0100

    fix: DocTypes aus Vereinsverwaltung werden im Modul Bestellwesen angezeigt (LANDA-374)

---
+++ landa/landa_sales/doctype/landa_sales_summy/landa_sales_summy.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Real Experts GmbH and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('LANDA Sales Summy', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ landa/landa_sales/doctype/landa_sales_summy/test_landa_sales_summy.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, Real Experts GmbH and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestLANDASalesSummy(unittest.TestCase):
+	pass
