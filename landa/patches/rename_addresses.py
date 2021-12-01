ad4d6f4f79f521dddf2fdb31f18131e09fa20798
barredterra <14891507+barredterra@users.noreply.github.com>
2021-12-01 01:56:41 +0100

    feat: add timer

--- landa/patches/rename_addresses.py
+++ landa/patches/rename_addresses.py
@@ -1,3 +1,5 @@
+from timeit import default_timer as timer
+
 import frappe
 from frappe.utils import update_progress_bar
 
@@ -8,9 +10,20 @@ def execute():
 	doctype = "Address"
 	all_addresses = frappe.get_all(doctype)
 	total_addresses = len(all_addresses)
+	last_duration = 0.0
+	total_duration = 0.0
 
 	for counter, address in enumerate(all_addresses):
-		update_progress_bar("Renaming Addresses", counter, total_addresses)
+		start_time = timer()
+		avg_seconds = total_duration / (counter or 1)
+		remaining_hours = round(
+			avg_seconds * (total_addresses - counter) / 60 / 60, 2
+		)
+		update_progress_bar(
+			f"Renaming Addresses ({round(avg_seconds)}s avg, {remaining_hours}h remaining)",
+			counter,
+			total_addresses,
+		)
 		doc = frappe.get_doc(doctype, address.name)
 
 		old_name = address.name
@@ -37,3 +50,7 @@ def execute():
 
 		if counter > 0 and counter % 100 == 0:
 			frappe.db.commit()
+
+		end_time = timer()
+		last_duration = end_time - start_time
+		total_duration += last_duration
