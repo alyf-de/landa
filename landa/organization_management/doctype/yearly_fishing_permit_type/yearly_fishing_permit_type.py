2060fd75d6044d3f6fd0680871e5239111079be6
barredterra <14891507+barredterra@users.noreply.github.com>
2021-12-20 18:29:43 +0100

    refactor: Yearly Fishing Permit Type

--- landa/organization_management/doctype/yearly_fishing_permit_type/yearly_fishing_permit_type.py
+++ landa/organization_management/doctype/yearly_fishing_permit_type/yearly_fishing_permit_type.py
@@ -8,49 +8,57 @@ from frappe import _
 from frappe.utils.data import get_link_to_form
 from frappe.model.document import Document
 
+
 class YearlyFishingPermitType(Document):
-	def before_insert(self):
-		self.short_code = self.short_code.upper()
+	def validate(self):
+		validate_item_attribute(self.item_attribute, self.title, self.name)
 
 	def on_submit(self):
-		add_attribute_value(
-			"Erlaubnisscheinart",
-			self.yearly_fishing_permit_type_name,
-			self.short_code
-		)
+		add_attribute_value(self.item_attribute, self.title, self.name)
 
-def add_attribute_value(name, attribute_value, abbr):
-	create_item_attribute(name)
-	item_attr = frappe.get_doc("Item Attribute", name)
-	item_attr_link = get_link_to_form(item_attr.doctype, item_attr.name)
-	item_attribute_values = [(row.attribute_value, row.abbr) for row in item_attr.item_attribute_values]
 
-	existing_pairs = [row for row in item_attribute_values if row == (attribute_value, abbr)]
-	if existing_pairs:
+def validate_item_attribute(item_attribute_name, attribute_value, abbr):
+	item_attr = frappe.get_doc("Item Attribute", item_attribute_name)
+	item_attribute_values = rows_to_tuples(item_attr.item_attribute_values)
+
+	if (attribute_value, abbr) in item_attribute_values:
 		# Item attribute exists already, as intended
 		return
 
 	existing_values = [row[0] for row in item_attribute_values]
 	existing_abbreviations = [row[1] for row in item_attribute_values]
+	item_attr_link = get_link_to_form(item_attr.doctype, item_attr.name)
 
 	if attribute_value in existing_values:
 		# Attribute value exists, but with a different abbreviation
-		frappe.throw(_('Value "{}" exists already in Item Attribute {}.').format(attribute_value, item_attr_link))
+		frappe.throw(
+			_('Value "{}" exists already in Item Attribute {}.').format(
+				attribute_value, item_attr_link
+			)
+		)
 
 	if abbr in existing_abbreviations:
 		# Abbreviation exists, but with a different attribute value
-		frappe.throw(_('Abbreviation "{}" exists already in Item Attribute {}.').format(abbr, item_attr_link))
+		frappe.throw(
+			_('Abbreviation "{}" exists already in Item Attribute {}.').format(
+				abbr, item_attr_link
+			)
+		)
 
-	item_attr.append('item_attribute_values', {
-		'attribute_value': attribute_value,
-		'abbr': abbr
-	})
+
+def rows_to_tuples(rows):
+	return [(row.attribute_value, row.abbr) for row in rows]
+
+
+def add_attribute_value(item_attribute_name, attribute_value, abbr):
+	"""Add a row (attribute_value, abbr) to Item Attibute."""
+	item_attr = frappe.get_doc("Item Attribute", item_attribute_name)
+	item_attribute_values = rows_to_tuples(item_attr.item_attribute_values)
+	if (attribute_value, abbr) in item_attribute_values:
+		# Item attribute exists already, as intended
+		return
+
+	item_attr.append(
+		"item_attribute_values", {"attribute_value": attribute_value, "abbr": abbr}
+	)
 	item_attr.save()
-
-
-def create_item_attribute(name):
-	"""Create an Item Attribute if there isn't one already"""
-	if not frappe.db.exists("Item Attribute", name):
-		item_attr = frappe.new_doc("Item Attribute")
-		item_attr.attribute_name = name
-		item_attr.insert()
