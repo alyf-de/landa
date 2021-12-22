c591c57435d782db34c0fb719b50ab79b4027992
barredterra <14891507+barredterra@users.noreply.github.com>
2021-12-22 21:21:52 +0100

    refactor: member data import

--- landa/organization_management/doctype/member_data_import/member_data_import.py
+++ landa/organization_management/doctype/member_data_import/member_data_import.py
@@ -4,33 +4,9 @@
 import frappe
 from frappe.model.document import Document
 from frappe.utils.dateutils import parse_date
-
+from landa.organization_management.doctype.landa_member.landa_member import LANDAMember
 
 class MemberDataImport(Document):
-
-	DATA_FIELDS = [
-		"member",
-		"last_name",
-		"first_name",
-		"address",
-		"address_line1",
-		"pincode",
-		"city",
-		"organization",
-		"year",
-		"type",
-		"number",
-		"additional_information",
-	]
-
-	CHECK_FIELDS = [
-		"is_supporting_member",
-		"has_key",
-		"has_special_yearly_fishing_permit_1",
-		"has_special_yearly_fishing_permit_2",
-	]
-
-	DATE_FIELDS = ["date_of_birth", "date_of_issue"]
 
 	MEMBER_FIELDS = [
 		"first_name",
@@ -40,18 +16,19 @@ class MemberDataImport(Document):
 		"has_key",
 		"has_special_yearly_fishing_permit_1",
 		"has_special_yearly_fishing_permit_2",
+		"additional_information",
 	]
 
 	ADDRESS_FIELDS = ["address_line1", "pincode", "city"]
 
-	def get(self, fieldname):
-		return super(MemberDataImport, self).get(fieldname) or ""
+	def before_insert(self):
+		self.preprocess()
 
 	def db_insert(self):
-		self.preprocess()
-		self.process_member()
-		self.process_address()
-		self.process_fishing_permit()
+		self.create_or_update_member()
+		self.create_or_update_address()
+		self.create_permit()
+		return {}
 
 	def load_from_db(self):
 		pass
@@ -64,29 +41,27 @@ class MemberDataImport(Document):
 
 	def preprocess(self):
 		"""Remove whitespaces, parse checkbox values and dates."""
-		for field in self.DATA_FIELDS:
-			self.set(field, self.get(field).strip())
-
-		for field in self.CHECK_FIELDS:
-			scrubbed = self.get(field).strip().lower()
-			if scrubbed in ["1", "ja", "j", "yes"]:
-				self.set(field, 1)
-			elif scrubbed in ["0", "nein", "n", "no", "-"]:
-				self.set(field, 0)
-			else:
-				self.set(field, None)
-
-		for field in self.DATE_FIELDS:
-			if not self.get(field):
+		for field in self.meta.fields:
+			value = self.get(field.fieldname)
+			if not isinstance(value, str):
 				continue
 
-			self.set(field, parse_date(self.get(field)))
+			if field.fieldtype in ("Data", "Link"):
+				self.set(field.fieldname, value.strip())
+			elif field.fieldtype == "Check":
+				self.set(field.fieldname, parse_checkbox_value(value))
+			elif field.fieldtype == "Date":
+				if not value:
+					continue
+				self.set(field.fieldname, parse_date(value))
+			elif field.fieldtype == "Int":
+				self.set(field.fieldname, int(value))
 
-	def process_member(self):
+	def create_or_update_member(self):
 		if self.member:
 			member_doc = frappe.get_doc("LANDA Member", self.member)
 		elif self.organization and self.last_name:
-			member_doc = self.create_member()
+			member_doc = create_member(self.organization, self.last_name)
 			self.member = member_doc.name
 		else:
 			frappe.throw(
@@ -95,77 +70,103 @@ class MemberDataImport(Document):
 
 		self.update_doc(member_doc, self.MEMBER_FIELDS)
 
-	def process_address(self):
+	def create_or_update_address(self):
 		if self.address_name:
 			address_doc = frappe.get_doc("Address", self.address_name)
 			self.update_doc(address_doc, self.ADDRESS_FIELDS)
 		elif all([self.address_line1, self.pincode, self.city]):
-			self.create_address()
+			create_address(
+				address_line1=self.address_line1,
+				pincode=self.pincode,
+				city=self.city,
+				member=self.member,
+			)
 
-	def process_fishing_permit(self):
+	def create_permit(self):
 		if not self.year:
 			return
 
 		doctype = "Yearly Fishing Permit Type"
 		default_type = "ALLG"
-		if (not self.type or not frappe.db.exists(doctype, self.type)):
+		if not self.type or not frappe.db.exists(doctype, self.type):
 			if frappe.db.exists(doctype, default_type):
 				self.type = default_type
 			else:
 				return
 
-		self.create_yearly_fishing_permit()
+		create_yearly_fishing_permit(
+			member=self.member,
+			year=self.year,
+			permit_type=self.type,
+			number=self.number,
+			date_of_issue=self.date_of_issue,
+		)
 
-	def create_member(self):
-		"""Return a new LANDA Member."""
-		member = frappe.new_doc("LANDA Member")
-		member.organization = self.organization
-		member.last_name = self.last_name
-
-		return member.insert()
-
-	def update_doc(self, doc, fields):
+	def update_doc(self, doc: Document, fields: "list[str]"):
 		"""Update all `fields` of `doc` with the values from `self`."""
-		for field in fields:
-			new_value = self.get(field)
-			if not new_value and not type(new_value, int): # int == 0 is allowed to disable checkbox
+		for fieldname in fields:
+			new_value = self.get(fieldname)
+			if not new_value and not isinstance(
+				new_value, int
+			):	# int == 0 is allowed to disable checkbox
 				continue
 
-			old_value = doc.get(field)
-			if field in self.DATE_FIELDS:
-				old_value = old_value.isoformat() if old_value else ""
-			elif field in self.DATA_FIELDS:
+			old_value = doc.get(fieldname)
+			fieldtype = doc.meta.get_field(fieldname).fieldtype
+
+			if fieldtype == "Date":
+				old_value = old_value.date().isoformat() if old_value else ""
+			elif fieldtype == "Data":
 				old_value = (old_value or "").strip()
 
 			if old_value != new_value:
-				doc.set(field, new_value)
+				doc.set(fieldname, new_value)
 
 		doc.save()
 
-	def create_address(self):
-		"""Return a new Address linked to LANDA Member."""
-		address = frappe.new_doc("Address")
-		address.address_type = "Personal"
-		address.address_line1 = self.address_line1
-		address.pincode = self.pincode
-		address.city = self.city
-		address.country = "Germany"
-		address.is_primary_address = 1
-		address.is_shipping_address = 1
-		address.append(
-			"links", {"link_doctype": "LANDA Member", "link_name": self.member}
-		)
 
-		return address.insert()
+def create_member(organization: str, last_name: str) -> LANDAMember:
+	"""Return a new LANDA Member."""
+	member = frappe.new_doc("LANDA Member")
+	member.organization = organization
+	member.last_name = last_name
 
-	def create_yearly_fishing_permit(self):
-		yfp = frappe.new_doc("Yearly Fishing Permit")
-		yfp.member = self.member
-		yfp.year = int(self.year)
-		yfp.type = self.type.upper()
-		yfp.number = self.number
-		yfp.date_of_issue = self.date_of_issue
+	return member.insert()
 
-		yfp.insert()
-		yfp.submit()
-		return yfp
+
+def create_address(address_line1: str, pincode: str, city: str, member: str) -> None:
+	"""Return a new Address linked to LANDA Member."""
+	address = frappe.new_doc("Address")
+	address.address_type = "Personal"
+	address.address_line1 = address_line1
+	address.pincode = pincode
+	address.city = city
+	address.country = "Germany"
+	address.is_primary_address = 1
+	address.is_shipping_address = 1
+	address.append("links", {"link_doctype": "LANDA Member", "link_name": member})
+
+	address.insert()
+
+
+def create_yearly_fishing_permit(
+	member: str, year: int, permit_type: str, number: str, date_of_issue
+) -> None:
+	yfp = frappe.new_doc("Yearly Fishing Permit")
+	yfp.member = member
+	yfp.year = year
+	yfp.type = permit_type
+	yfp.number = number
+	yfp.date_of_issue = date_of_issue
+
+	yfp.insert()
+	yfp.submit()
+
+
+def parse_checkbox_value(value: str) -> int:
+	scrubbed = value.strip().lower()
+
+	if scrubbed in ["1", "ja", "j", "yes", "y"]:
+		return 1
+	else:
+		return 0
