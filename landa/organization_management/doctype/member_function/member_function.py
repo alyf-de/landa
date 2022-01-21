f7d18785d79ec3a639d65afee397952a52c3a4ac
barredterra <14891507+barredterra@users.noreply.github.com>
2022-01-21 11:34:53 +0100

    refactor: member function

--- landa/organization_management/doctype/member_function/member_function.py
+++ landa/organization_management/doctype/member_function/member_function.py
@@ -11,67 +11,78 @@ from frappe.utils.data import today
 from frappe.utils.data import date_diff
 from frappe.model.document import Document
 
-class MemberFunction(Document):
 
+class MemberFunction(Document):
 	def before_validate(self):
 		self.update_is_active()
-	
+
 	def validate(self):
 		if self.start_date and self.end_date:
 			if date_diff(self.start_date, self.end_date) > 0:
-				frappe.throw(_('End Date cannot be before Start Date.'))
+				frappe.throw(_("End Date cannot be before Start Date."))
 
 		self.validate_member_function_category()
 
-
 	def validate_member_function_category(self):
-		has_role = lambda role: role in frappe.get_roles(frappe.session.user)
-		access_level = frappe.get_value('Member Function Category', self.member_function_category, 'access_level')
+		def has_role(role):
+			return role in frappe.get_roles(frappe.session.user)
 
-		if access_level == 'State Organization' and has_role('LANDA State Organization Employee'):
-			return
+		access_level = frappe.get_value(
+			"Member Function Category", self.member_function_category, "access_level"
+		)
 
-		if (
-			access_level == 'Regional Organization'
-			and (
-				has_role('LANDA State Organization Employee')
-				or has_role('LANDA Regional Organization Management')
-			)
+		if access_level == "State Organization" and has_role(
+			"LANDA State Organization Employee"
 		):
 			return
 
-		if access_level == 'Local Organization':
+		if access_level == "Regional Organization" and (
+			has_role("LANDA State Organization Employee")
+			or has_role("LANDA Regional Organization Management")
+		):
+			return
+
+		if access_level == "Local Organization":
 			return
 
 		frappe.throw(
-			_("No permission to set Member Function Category {0}").format(self.member_function_category),
-			frappe.PermissionError
+			_("No permission to set Member Function Category {0}").format(
+				self.member_function_category
+			),
+			frappe.PermissionError,
 		)
 
 	def on_update(self):
 		self.update_user_roles()
 
 	def on_trash(self):
-		self.status = 'Inactive'
+		self.status = "Inactive"
 		self.update_user_roles()
 
 	def update_user_roles(self):
-		member_function_category = frappe.get_doc("Member Function Category", self.member_function_category)
-		if self.status == 'Active':
+		member_function_category = frappe.get_doc(
+			"Member Function Category", self.member_function_category
+		)
+		if self.status == "Active":
 			member_function_category.add_roles_and_permissions(self.member)
 		else:
-			member_function_category.remove_roles_and_permissions(self.member, disabled_member_function=self.name)
+			member_function_category.remove_roles_and_permissions(
+				self.member, disabled_member_function=self.name
+			)
 
 	def update_is_active(self):
-		in_past = lambda date: date_diff(today(), date) > 0
-		in_future = lambda date: date_diff(today(), date) < 0
-
-		if self.start_date and in_future(self.start_date):
-			self.status = 'Planned'
-		elif self.end_date and in_past(self.end_date):
-			self.status = 'Inactive'
+		if self.is_planned():
+			self.status = "Planned"
+		elif self.is_inactive():
+			self.status = "Inactive"
 		else:
-			self.status = 'Active'
+			self.status = "Active"
+
+	def is_planned(self):
+		return self.start_date and date_diff(today(), self.start_date) < 0
+
+	def is_inactive(self):
+		return self.end_date and date_diff(today(), self.end_date) > 0
 
 
 def disable_expired_member_functions():
@@ -81,24 +92,25 @@ def disable_expired_member_functions():
 
 
 def apply_active_member_functions(filters):
-	for member_function in get_active_member_functions(filters=filters, pluck='name'):
+	for member_function in get_active_member_functions(filters=filters, pluck="name"):
 		doc = frappe.get_doc("Member Function", member_function)
 		doc.save()
 
 
 def get_expired_member_functions():
-	return frappe.get_all('Member Function', filters=[
-		['end_date', '<', today()],
-		['end_date', 'is', 'set'],
-	])
-
-
-def get_active_member_functions(filters: dict=None, pluck: str=None):
-	return frappe.get_all('Member Function',
-		filters=filters,
-		or_filters=[
-			['end_date', 'is', 'not set'],
-			['end_date', '>=', today()]
+	return frappe.get_all(
+		"Member Function",
+		filters=[
+			["end_date", "<", today()],
+			["end_date", "is", "set"],
 		],
-		pluck=pluck
+	)
+
+
+def get_active_member_functions(filters: dict = None, pluck: str = None):
+	return frappe.get_all(
+		"Member Function",
+		filters=filters,
+		or_filters=[["end_date", "is", "not set"], ["end_date", ">=", today()]],
+		pluck=pluck,
 	)
