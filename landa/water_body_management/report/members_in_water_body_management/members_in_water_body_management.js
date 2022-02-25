486ef02daff994712634195e61aad87f27067db2
melanieroedel <roedel@realexperts.de>
2022-02-25 14:03:41 +0100

    feat: full water body management contact report LANDA-37

--- landa/water_body_management/report/members_in_water_body_management/members_in_water_body_management.js
+++ landa/water_body_management/report/members_in_water_body_management/members_in_water_body_management.js
@@ -4,6 +4,30 @@
 
 frappe.query_reports["Members in Water Body Management"] = {
 	"filters": [
-
+           {
+            "fieldname": "water_body",
+            "fieldtype": "Link",
+            "options": "Water Body",
+            "label": __("Water Body"),
+            "mandatory": 0,
+            "wildcard_filter": 0
+           },
+           {
+            "fieldname": "organization",
+            "fieldtype": "Link",
+            "options": "Organization",
+            "label": __("Organization"),
+            "mandatory": 0,
+            "wildcard_filter": 0,
+            "default": frappe.defaults.get_user_default("Organization"),
+           },
+		   {
+            "fieldname": "fishing_area",
+            "fieldtype": "Link",
+            "options": "Fishing Area",
+            "label": __("Fishing Area"),
+            "mandatory": 0,
+            "wildcard_filter": 0
+           }
 	]
 };
