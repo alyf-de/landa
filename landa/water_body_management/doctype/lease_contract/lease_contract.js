2602dcc4ba20c823d9475554c515928199a73416
melanieroedel <roedel@realexperts.de>
2022-02-25 15:43:16 +0100

    feat: add lease contract LANDA-552

---
+++ landa/water_body_management/doctype/lease_contract/lease_contract.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Real Experts GmbH and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Lease Contract', {
+	// refresh: function(frm) {
+
+	// }
+});
