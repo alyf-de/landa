e8daa5a3a348b2df6869db6fb7162f9c75f77715
barredterra <14891507+barredterra@users.noreply.github.com>
2022-03-13 22:16:16 +0100

    feat: customize initial map view

--- landa/hooks.py
+++ landa/hooks.py
@@ -167,7 +167,7 @@ welcome_email = "landa.utils.welcome_email"
 
 # include js, css files in header of desk.html
 # app_include_css = "/assets/landa/css/landa.css"
-# app_include_js = "/assets/landa/js/landa.js"
+app_include_js = "/assets/landa/js/map_defaults.js"
 
 # include js, css files in header of web template
 # web_include_css = "/assets/landa/css/landa.css"
