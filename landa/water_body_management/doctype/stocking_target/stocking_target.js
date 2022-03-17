31705e705fd8d2f175458590b0b8218a76da1d5d
barredterra <14891507+barredterra@users.noreply.github.com>
2022-03-18 00:26:55 +0100

    feat: stocking target only for own regional organization (LANDA-587)

--- landa/water_body_management/doctype/stocking_target/stocking_target.js
+++ landa/water_body_management/doctype/stocking_target/stocking_target.js
@@ -2,6 +2,22 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Stocking Target", {
+	onload: function (frm) {
+		frm.set_query("organization", function (doc) {
+			return {
+				filters: {
+					parent_organization: "LV",
+				},
+			};
+		});
+		frm.set_query("water_body", function (doc) {
+			return {
+				filters: {
+					organization: doc.organization,
+				},
+			};
+		});
+	},
 	refresh: function (frm) {
 		frm.trigger("weight");
 		frm.trigger("quantity");
