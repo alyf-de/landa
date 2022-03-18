68dd038aa1cff9fc3cf952432e7049f5a6c23717
barredterra <14891507+barredterra@users.noreply.github.com>
2022-03-18 14:28:34 +0100

    feat: set year of fishing permit (LANDA-531)

--- landa/organization_management/doctype/yearly_fishing_permit/yearly_fishing_permit.js
+++ landa/organization_management/doctype/yearly_fishing_permit/yearly_fishing_permit.js
@@ -1,14 +1,27 @@
 // Copyright (c) 2021, Real Experts GmbH and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Yearly Fishing Permit', {
-	setup: function(frm) {
-		frm.set_query('type', function() {
+frappe.ui.form.on("Yearly Fishing Permit", {
+	onload: function (frm) {
+		frm.set_query("type", function () {
 			return {
 				filters: {
-					'docstatus': ["!=","0"]
-				}
-			}
-		})
-	}
+					docstatus: ["!=", "0"],
+				},
+			};
+		});
+		if (frm.is_new() && !frm.doc.year) {
+			frm.trigger("set_default_year");
+		}
+	},
+	set_default_year: function (frm) {
+		// set default year to the current year if it's before november,
+		// else to the next year
+		const now = moment();
+		let year = now.year();
+		if (now.month() >= 11) {
+			year += 1;
+		}
+		frm.set_value("year", year);
+	},
 });
