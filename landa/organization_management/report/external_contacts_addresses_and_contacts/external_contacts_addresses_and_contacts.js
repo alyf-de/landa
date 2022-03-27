aa430dfe8050e3a95cbee3ae3e052541f7c54727
melanieroedel <roedel@realexperts.de>
2022-03-27 15:27:06 +0200

    feat: add script report landa-546

---
+++ landa/organization_management/report/external_contacts_addresses_and_contacts/external_contacts_addresses_and_contacts.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2022, Real Experts GmbH and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["External Contacts Addresses and Contacts"] = {
+	"filters": [
+
+	]
+};
