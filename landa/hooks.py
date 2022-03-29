91c8449909e6066b7fa18a7185505c57e2ece947
barredterra <14891507+barredterra@users.noreply.github.com>
2022-03-30 01:31:23 +0200

    fix: remove workflow state "Rejected" (LANDA-597)

--- landa/hooks.py
+++ landa/hooks.py
@@ -51,9 +51,9 @@ fixtures = [
 	},
 	"Salutation",
 	"Gender",
-   {"dt": "Note", "filters": {"name": "Datenschutz Hinweise"}},
+	{"dt": "Note", "filters": {"name": "Datenschutz Hinweise"}},
 	"Workflow State",
-	"Workflow Action",
+	"Workflow Action Master",
 	"Workflow",
 ]
 
