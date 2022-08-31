56c292b879077edaa437f3d6d550df5588f58f9d
barredterra <14891507+barredterra@users.noreply.github.com>
2022-08-31 19:40:53 +0200

    feat(Delivery Note): prefill items (LANDA-659)

--- landa/landa_stock/delivery_note/delivery_note.js
+++ landa/landa_stock/delivery_note/delivery_note.js
@@ -1,22 +1,18 @@
-frappe.ui.form.on('Delivery Note',  {
+frappe.ui.form.on("Delivery Note",  {
     refresh: function (frm) {
-        frm.trigger('set_item_query');
-    },
-    year_of_settlement: function (frm) {
-        frm.clear_table("items");
-        frm.refresh_field("items");
+        frm.trigger("set_item_query");
     },
     set_item_query: function(frm) {
         frm.set_query("item_code", "items", function () {
             const filters = {
-                'valid_from_year': ["<=", frm.doc.year_of_settlement],
-                'valid_to_year': [">=", frm.doc.year_of_settlement],
-                'is_sales_item': 1
+                valid_from_year: ["<=", frm.doc.year_of_settlement],
+                valid_to_year: [">=", frm.doc.year_of_settlement],
+                is_sales_item: 1
             }
 
             if (frm.doc.is_return) {
                 // some items cannot be returned
-                filters['cannot_be_returned'] = 0;
+                filters["cannot_be_returned"] = 0;
             }
 
             return {
@@ -24,5 +20,10 @@ frappe.ui.form.on('Delivery Note',  {
                 filters: filters
             };
         });
-    }
+    },
+    year_of_settlement: function (frm) {
+        // don't prefill items if the table is already populated
+        if (frm.doc.items[0].item_code || frm.doc.items.length > 1) return;
+        landa.prefill_items(frm);
+    },
 });
