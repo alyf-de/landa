cc977a2734efcaeccf32b09322fb4d8ca2e709c1
barredterra <14891507+barredterra@users.noreply.github.com>
2022-08-31 19:40:13 +0200

    refactor: extract prefill_items into a utility function (LANDA-659)

--- landa/landa_sales/sales_order/sales_order.js
+++ landa/landa_sales/sales_order/sales_order.js
@@ -1,14 +1,14 @@
 // Copyright (c) 2021, Real Experts GmbH and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Sales Order',  {
+frappe.ui.form.on("Sales Order",  {
     refresh: function (frm) {
-        frm.set_query('item_code', 'items', function() {
+        frm.set_query("item_code", "items", function(doc) {
             return {
-                query: 'erpnext.controllers.queries.item_query',
+                query: "erpnext.controllers.queries.item_query",
                 filters: {
-                    valid_from_year: ['<=', frm.doc.year_of_settlement],
-                    valid_to_year: ['>=', frm.doc.year_of_settlement],
+                    valid_from_year: ["<=", doc.year_of_settlement],
+                    valid_to_year: [">=", doc.year_of_settlement],
                     cannot_be_ordered: 0,
                     is_sales_item: 1
                 }
@@ -21,54 +21,16 @@ frappe.ui.form.on('Sales Order',  {
         });
     },
     year_of_settlement: function (frm) {
-        frm.trigger('prefill_items');
-        // the month is 0-indexed
-        frm.doc.delivery_date = new Date(frm.doc.year_of_settlement, 11, 31);
+        landa.prefill_items(frm);
+        frm.doc.delivery_date = new Date(frm.doc.year_of_settlement, 11, 31); // the month is 0-indexed
     },
-    prefill_items: function (frm) {
-        if (frm.doc.customer && frm.doc.year_of_settlement) {
-            frappe.call({
-                method: 'erpnext.controllers.queries.item_query', // Use item query to get all selectable items
-                args: {
-                    doctype: 'Item',
-                    txt: '', // empty search text returns all items
-                    searchfield: 'name',
-                    start: 0,
-                    page_len: 99, // limit pre-filled items to 100
-                    filters: {
-                        valid_from_year: ['<=', frm.doc.year_of_settlement],
-                        valid_to_year: ['>=', frm.doc.year_of_settlement],
-                        cannot_be_ordered: 0,
-                        is_sales_item: 1
-                    },
-                    as_dict: true
-                },
-                callback: function (r) {
-                    frm.clear_table('items');
-                    frm.refresh_field('items');
-
-                    for (const item of r.message) {
-                        const row = frm.add_child('items');
-                        frappe.model.set_value(row.doctype, row.name, 'item_code', item.name);
-
-                        // `ControlLink.validate()` does not get triggered by `frappe.model.set_value`,
-                        // therefore we have to set the "fetch from"-fields manually.
-                        frappe.model.set_value(row.doctype, row.name, 'cannot_be_returned', item.cannot_be_returned);
-                    }
-                }
-            });
-        } else {
-            frm.clear_table('items');
-            frm.refresh_field('items');
-        }
-    }
 });
 
-frappe.ui.form.on('Sales Order Item', {
+frappe.ui.form.on("Sales Order Item", {
     price_list_rate: function (frm, cdt, cdn) {
         // `price_list_rate` is set only once, when item details are loaded. We
         // use this as an indicator to see that all item details are complete.
         // Then we reset the quantity from 1 to 0.
-        frappe.model.set_value(cdt, cdn, 'qty', 0);
+        frappe.model.set_value(cdt, cdn, "qty", 0);
     }
 });
