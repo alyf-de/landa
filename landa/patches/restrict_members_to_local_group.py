a4484b3885f19ff4526dd41b38809ddf5b0f1092
barredterra <14891507+barredterra@users.noreply.github.com>
2022-09-09 17:17:52 +0200

    patch: restrict embers to local group (LANDA-661)

---
+++ landa/patches/restrict_members_to_local_group.py
@@ -0,0 +1,20 @@
+from pypika.functions import Length
+import frappe
+from frappe.permissions import clear_user_permissions_for_doctype, add_user_permission
+from landa.organization_management.doctype.member_function.member_function import (
+	apply_active_member_functions,
+)
+
+
+def execute():
+	table = frappe.qb.DocType("LANDA Member")
+	for member_name, user, organization in (
+		frappe.qb.from_(table)
+		.select(table.name, table.user, table.organization)
+		.where(Length(table.organization) > 7)
+		.where(table.user.notnull())
+		.run()
+	):
+		clear_user_permissions_for_doctype("Organization", user)
+		add_user_permission("Organization", organization, user, ignore_permissions=True)
+		apply_active_member_functions({"member": member_name})
