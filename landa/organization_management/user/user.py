ed4bc1fe19d0bc3888c2046501bc129b3cdf2e12
barredterra <14891507+barredterra@users.noreply.github.com>
2022-10-19 19:33:25 +0200

    feat: delete Route History and Access Log along with user (LANDA-285)

--- landa/organization_management/user/user.py
+++ landa/organization_management/user/user.py
@@ -1,7 +1,9 @@
 from frappe.core.doctype.user.user import User
-from landa.utils import remove_from_table, db_unset
+from landa.utils import remove_from_table, db_unset, db_delete
 
 
 def on_trash(user: User, event: str) -> None:
+	db_delete("Route History", "user", user.name)
+	db_delete("Access Log", "user", user.name)
 	db_unset("LANDA Member", "user", user.name)
 	remove_from_table("Note", "seen_by", {"user": user.name})
