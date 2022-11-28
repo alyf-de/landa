b3b1a8830cb6f3b0c095bacd54ff895aee115980
barredterra <14891507+barredterra@users.noreply.github.com>
2022-11-28 18:45:03 +0100

    perf: don't save contacts that have not been modified

--- landa/patches/set_phone_and_email_as_primary.py
+++ landa/patches/set_phone_and_email_as_primary.py
@@ -19,9 +19,10 @@ def execute():
 	for i, contact_name in enumerate(contacts):
 		update_progress_bar("Setting primary email and phone", i, total)
 		contact = frappe.get_doc("Contact", contact_name)
-		set_primary_email_if_missing(contact)
-		set_primary_phone_if_missing(contact)
-		contact.save(ignore_permissions=True, ignore_version=True)
+		modified = set_primary_email_if_missing(contact)
+		modified |= set_primary_phone_if_missing(contact)
+		if modified:
+			contact.save(ignore_permissions=True, ignore_version=True)
 
 	frappe.db.auto_commit_on_many_writes = False
 
@@ -34,29 +35,35 @@ def number_is_mobile(number: str) -> bool:
 	return "".join(filter(str.isdigit, number)).startswith(("01", "491", "00491"))
 
 
-def set_primary_email_if_missing(contact: Contact) -> None:
+def set_primary_email_if_missing(contact: Contact) -> bool:
 	"""If no email_id is set as primary for a contact, set the first email_id as primary"""
 	if not contact.email_ids or value_is_set(contact.email_ids, "is_primary"):
-		return
+		return False
 
 	contact.email_ids[0].is_primary = 1
+	return True
 
 
-def set_primary_phone_if_missing(contact: Contact) -> None:
+def set_primary_phone_if_missing(contact: Contact) -> bool:
 	"""If no phone is set as primary mobile or primary phone for a contact,
 	set the first mobile as primary mobile and the first not mobile as primary phone
 	(according to the first digits of the phone number)"""
+	modified = False
 	if not contact.phone_nos:
-		return
+		return modified
 
 	if not value_is_set(contact.phone_nos, "is_primary_mobile_no"):
 		for phone in contact.phone_nos:
 			if number_is_mobile(phone.phone):
 				phone.is_primary_mobile_no = 1
+				modified = True
 				break
 
 	if not value_is_set(contact.phone_nos, "is_primary_phone"):
 		for phone in contact.phone_nos:
 			if not number_is_mobile(phone.phone):
 				phone.is_primary_phone = 1
+				modified = True
 				break
+
+	return modified
