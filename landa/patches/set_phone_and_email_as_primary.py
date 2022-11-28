4e7c3a73bd644533323634697dc969894013ab5e
melanieroedel <79863208+melanieroedel@users.noreply.github.com>
2022-11-28 17:36:48 +0100

    fix: set only one phone number as primary via API do not write directly not directly into database

--- landa/patches/set_phone_and_email_as_primary.py
+++ landa/patches/set_phone_and_email_as_primary.py
@@ -11,6 +11,11 @@ def execute():
 		cleared_number = remove_non_digit_characters(number)
 		return cleared_number.startswith(("01","491","00491"))
 
+	def set_doc_attr(doctype,name,field,value):
+		doc=frappe.get_doc(doctype, name)
+		setattr(doc, field, value)
+		doc.save()
+
 	def set_primary_email_if_missing(contact):
 		"""If no email_id is set as primary for a contact, set the first email_id as primary"""
 		contact_emails = frappe.get_list(
@@ -19,13 +24,12 @@ def execute():
 			fields=["name", "is_primary"],
 		)
 		if contact_emails and value_not_set(contact_emails, "is_primary"):
-			doc=frappe.get_doc("Contact Email", contact_emails[0]["name"])
-			setattr(doc, "is_primary", 1)
-			doc.save()
+			set_doc_attr("Contact Email",contact_emails[0]["name"],"is_primary", 1)
 
 	def set_primary_phone_if_missing(contact):
 		"""If no phone is set as primary mobile or primary phone for a contact,
-		set all phones as primary phone or primary phone according to the first digits of the phone number"""
+		set the first mobile as primary mobile and the first not mobile as primary phone 
+		(according to the first digits of the phone number)"""
 		contact_phones = frappe.get_list(
 			"Contact Phone",
 			filters={"parent": contact},
@@ -34,18 +38,17 @@ def execute():
 		if contact_phones and value_not_set(contact_phones, "is_primary_mobile_no") and value_not_set(
 			contact_phones, "is_primary_phone"
 		):
+			mobile_is_set=False
+			phone_is_set=False
 			for contact_phone in contact_phones:
 				if number_is_mobile(contact_phone["phone"]):
-					frappe.db.set_value(
-						"Contact Phone",
-						contact_phone["name"],
-						"is_primary_mobile_no",
-						1,
-					)
+					if not mobile_is_set:
+						set_doc_attr("Contact Phone",contact_phone["name"],"is_primary_mobile_no", 1)
+						mobile_is_set=True
 				else:
-					frappe.db.set_value(
-						"Contact Phone", contact_phone["name"], "is_primary_phone", 1
-					)
+					if not phone_is_set:
+						set_doc_attr("Contact Phone",contact_phone["name"],"is_primary_phone", 1)
+						phone_is_set=True
 
 	contacts = frappe.get_list("Contact", pluck="name")
 	for contact in contacts:
