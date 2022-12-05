56a5b33a50bf36aa459e1043e27f4703df9aad3b
barredterra <14891507+barredterra@users.noreply.github.com>
2022-12-05 20:06:43 +0100

    feat: set only once for link to LANDA Member

--- landa/organization_management/user/user.js
+++ landa/organization_management/user/user.js
@@ -6,6 +6,15 @@ frappe.ui.form.on("User", {
 			&& frm.doc.name !== frappe.session.user
 		) {
 			frm.set_df_property("enabled", "read_only", 0);
+		} else {
+			frm.set_df_property("enabled", "read_only", 1);
+		}
+
+		// set link to LANDA Member only once
+		if (frm.doc.landa_member) {
+			frm.set_df_property("landa_member", "read_only", 1);
+		} else {
+			frm.set_df_property("landa_member", "read_only", 0);
 		}
 	},
 });
