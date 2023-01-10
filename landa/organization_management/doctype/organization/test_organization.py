9bb6192742cfea772bd8b9b1a784d8de2c6db124
barredterra <14891507+barredterra@users.noreply.github.com>
2023-01-11 00:58:04 +0100

    feat: add test for organization naming

--- landa/organization_management/doctype/organization/test_organization.py
+++ landa/organization_management/doctype/organization/test_organization.py
@@ -3,8 +3,21 @@
 # See license.txt
 from __future__ import unicode_literals
 
-# import frappe
-import unittest
+import frappe
+from frappe.tests.utils import FrappeTestCase
 
-class TestOrganization(unittest.TestCase):
-	pass
+
+class TestOrganization(FrappeTestCase):
+	def setUp(self) -> None:
+		test_records = frappe.get_test_records("Organization")
+		for record in test_records:
+			frappe.get_doc(record).insert()
+
+	def test_naming(self):
+		orgs = frappe.get_all("Organization", pluck="name")
+
+		self.assertIn("ST", orgs)
+		self.assertIn("REG", orgs)
+		self.assertIn("REG-001", orgs)
+		self.assertIn("REG-002", orgs)
+		self.assertIn("REG-002-01", orgs)
