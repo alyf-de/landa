276bd7d7298638e0aef469c69cd78a05b3cb873d
barredterra <14891507+barredterra@users.noreply.github.com>
2023-01-20 03:00:04 +0100

    refactor: report Member Birthday List (LAN-719)
    - clean up birthday logic and add tests
    - simplify and speed up report

---
+++ landa/organization_management/birthday.py
@@ -0,0 +1,35 @@
+from datetime import date
+from calendar import isleap
+
+
+def date_has_passed(date_of_birth: date, at_date: date) -> bool:
+	return (at_date.month, at_date.day) >= (date_of_birth.month, date_of_birth.day)
+
+
+def get_age(date_of_birth: date = None, at_date: date = None) -> int:
+	if not date_of_birth:
+		return
+
+	at_date = at_date or date.today()
+	return at_date.year - date_of_birth.year - (not date_has_passed(date_of_birth, at_date))
+
+
+def get_upcoming_birthday(date_of_birth: date = None, at_date: date = None) -> date:
+	if not date_of_birth:
+		return
+
+	at_date = at_date or date.today()
+	year = at_date.year
+
+	if date_has_passed(date_of_birth, at_date):
+		year += 1
+
+	if date_of_birth.month == 2 and date_of_birth.day == 29 and not isleap(year):
+		return date(year, 3, 1)
+
+	return date(year, date_of_birth.month, date_of_birth.day)
+
+
+def next_birthday_is_decadal(age: int = None) -> bool:
+	age = age or 0
+	return (age + 1) % 10 == 0
