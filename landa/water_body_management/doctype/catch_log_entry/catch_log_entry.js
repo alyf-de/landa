ca5b5dafe2a0280734d684de72a4c96a5c945779
marination <maricadsouza221197@gmail.com>
2023-02-01 13:44:32 +0530

    feat: 'Save and New' on Catch Log Entry

--- landa/water_body_management/doctype/catch_log_entry/catch_log_entry.js
+++ landa/water_body_management/doctype/catch_log_entry/catch_log_entry.js
@@ -2,9 +2,23 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Catch Log Entry", {
-	onload: function (frm) {
+	onload: (frm) => {
 		if (frm.is_new() && !frm.doc.year) {
 			frm.set_value("year", moment().year() - 1);
 		}
 	},
+	refresh: (frm) => {
+		if (frm.doc.docstatus < 1 && frm.doc.__unsaved)	{
+			// to avoid primary action refresh on dirty form (setting field value)
+			frm.disable_save();
+
+			frm.page.set_primary_action(__("Save and New"), () => {
+				frappe.run_serially([
+				   () => frm.save(),
+				   () => frappe.new_doc("Catch Log Entry"),
+				   () => window.location.reload(),
+			   ]);
+		   })
+	   }
+   },
 });
