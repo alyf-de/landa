2de5ae42d2a9ad7046c63f443fa4d32d01446177
barredterra <14891507+barredterra@users.noreply.github.com>
2023-03-20 20:26:18 +0100

    fix: hide bulk delete for Member (LAN-91)

--- landa/organization_management/doctype/landa_member/landa_member_list.js
+++ landa/organization_management/doctype/landa_member/landa_member_list.js
@@ -1,7 +1,10 @@
 frappe.listview_settings["LANDA Member"] = {
 	onload(list_view) {
 		if (!frappe.user.has_role("System Manager")) {
-			list_view.page.actions.find("[data-label='Delete']").closest("li").addClass("hidden");
+			list_view.page.actions
+				.find(`[data-label='${encodeURIComponent(__("Delete"))}']`)
+				.closest("li")
+				.addClass("hidden");
 		}
 	},
 };
