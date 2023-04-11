b7f128bb5dd58c064ef2de8d67cafc140a55724d
barredterra <14891507+barredterra@users.noreply.github.com>
2023-04-11 19:31:18 +0200

    refactor: landa member

--- landa/organization_management/doctype/landa_member/landa_member.py
+++ landa/organization_management/doctype/landa_member/landa_member.py
@@ -6,35 +6,37 @@ from __future__ import unicode_literals
 
 import frappe
 from frappe import _
-from frappe.model.document import Document
 from frappe.contacts.address_and_contact import load_address_and_contact
-from frappe.model.naming import make_autoname
-from frappe.model.naming import revert_series_if_last
-
-from landa.utils import delete_records_linked_to, delete_dynamically_linked, get_member_and_organization
+from frappe.model.document import Document
+from frappe.model.naming import make_autoname, revert_series_if_last
+from landa.utils import (
+	delete_dynamically_linked,
+	delete_records_linked_to,
+	get_member_and_organization,
+)
 
 
 class LANDAMember(Document):
-
 	def autoname(self):
 		"""Generate the unique member number (name field)
 
-		Organization:				AVL-001-0001, AVE-001-0001, ...
-		Local group:				AVL-001-01-0001, AVL-001-02-0001, ...
+		Organization: AVL-001-0001, AVE-001-0001, ...
+		Local group: AVL-001-01-0001, AVL-001-02-0001, ...
 		"""
 		if self.name:
 			return
-
-		self.name = make_autoname(self.organization + '-.####', 'LANDA Member')
+		self.name = make_autoname(f"{self.organization}-.####", "LANDA Member")
 
 	def onload(self):
 		load_address_and_contact(self)
 
 	def validate(self):
-		organization_is_group = lambda: frappe.db.get_value('Organization', self.organization, 'is_group')
-
-		if organization_is_group():
-			frappe.throw(_('Cannot be a member of organization {} because it is a group.').format(self.organization))
+		if frappe.db.get_value("Organization", self.organization, "is_group"):
+			frappe.throw(
+				_(
+					"Cannot be a member of organization {} because it is a group."
+				).format(self.organization)
+			)
 
 		self.full_name = get_full_name(self.first_name, self.last_name)
 
@@ -45,7 +47,13 @@ class LANDAMember(Document):
 
 		user = frappe.db.exists("User", {"landa_member": self.name})
 		if user:
-			frappe.delete_doc("User", user, ignore_permissions=True, ignore_missing=True, delete_permanently=True)
+			frappe.delete_doc(
+				"User",
+				user,
+				ignore_permissions=True,
+				ignore_missing=True,
+				delete_permanently=True,
+			)
 
 		delete_dynamically_linked("Address", self.doctype, self.name)
 		delete_dynamically_linked("Contact", self.doctype, self.name)
@@ -56,10 +64,14 @@ class LANDAMember(Document):
 	def revert_series(self):
 		"""Decrease the naming counter when the newest member gets deleted."""
 		# reconstruct the key used to generate the name
-		number_part_len = len(self.name.split('-')[-1])
+		number_part_len = len(self.name.split("-")[-1])
 		key = f"{self.name[:-number_part_len]}.{'#' * number_part_len}"
 		revert_series_if_last(key, self.name)
 
 
-def get_full_name(first_name,last_name):
-	return (first_name or "") + (" " if (last_name and first_name) else "") + (last_name or "") 
+def get_full_name(first_name, last_name):
+	return (
+		(first_name or "")
+		+ (" " if (last_name and first_name) else "")
+		+ (last_name or "")
+	)
