db8cdf99db7241ee9d34d88f466e5cd2cbfdc552
diePuppe <tim.puppe@htw-dresden.de>
2023-04-19 11:12:24 +0200

    feat: update plausibility test (LAN -81)

--- landa/water_body_management/doctype/catch_log_entry/catch_log_entry.py
+++ landa/water_body_management/doctype/catch_log_entry/catch_log_entry.py
@@ -24,11 +24,17 @@ class CatchLogEntry(Document):
 	def validate(self):
 		tolerance = 0.4
 		for row in self.fish_catches:
-			typical_weight = frappe.db.get_value("Fish Species", row.fish_species, "typical_weight")
-			if (
-				row.weight_in_kg < typical_weight * (1 - tolerance) or
-				row.weight_in_kg > typical_weight * (1 + tolerance)
-			):
-				frappe.msgprint(
-					frappe._("The weight of {0} in row {1} is not plausible").format(row.fish_species, row.idx)
-				)
+			species = row.fish_species   # wurde dreimal aus der Datenbank geholt deswegen hier das auslesen in eine extra variable
+			if frappe.db.get_value("Fish Species", species, "typical_weight"):  								 # check if the weight is available
+				typical_weight = float(frappe.db.get_value("Fish Species", species, "typical_weight"))
+				average_weight = row.weight_in_kg / row.amount
+				if (
+					average_weight < typical_weight * (1 - tolerance) or
+					average_weight > typical_weight * (1 + tolerance)
+				):
+					frappe.msgprint(
+						frappe._("The weight of {0} in row {1} is not plausible").format(species, row.idx)
+					)
+			else:
+				pass
+				#automatisierte E-Mail an den Verantwortlichen das Gewicht nach zu pflegen?
