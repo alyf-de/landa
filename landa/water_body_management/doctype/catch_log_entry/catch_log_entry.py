dc29df88c8fc2e411b8cc798fff34e5793e907f9
diePuppe <95628567+diePuppe@users.noreply.github.com>
2023-04-21 09:21:15 +0200

    feat: update plausibility-test (LAN-81)
    Co-authored-by: Raffael Meyer <14891507+barredterra@users.noreply.github.com>

--- landa/water_body_management/doctype/catch_log_entry/catch_log_entry.py
+++ landa/water_body_management/doctype/catch_log_entry/catch_log_entry.py
@@ -24,17 +24,13 @@ class CatchLogEntry(Document):
 	def validate(self):
 		tolerance = 0.4
 		for row in self.fish_catches:
-			species = row.fish_species   # wurde dreimal aus der Datenbank geholt deswegen hier das auslesen in eine extra variable
-			if frappe.db.get_value("Fish Species", species, "typical_weight"):  								 # check if the weight is available
-				typical_weight = float(frappe.db.get_value("Fish Species", species, "typical_weight"))
-				average_weight = row.weight_in_kg / row.amount
-				if (
-					average_weight < typical_weight * (1 - tolerance) or
-					average_weight > typical_weight * (1 + tolerance)
-				):
-					frappe.msgprint(
-						frappe._("The weight of {0} in row {1} is not plausible").format(species, row.idx)
-					)
-			else:
-				pass
-				#automatisierte E-Mail an den Verantwortlichen das Gewicht nach zu pflegen?
+			typical_weight = flt(frappe.db.get_value("Fish Species", row.fish_species, "typical_weight_in_kg"))
+			average_weight = row.weight_in_kg / row.amount
+			if (
+				typical_weight and
+				average_weight < typical_weight * (1 - tolerance) or
+				average_weight > typical_weight * (1 + tolerance)
+			):
+				frappe.msgprint(
+					frappe._("The weight of {0} in row {1} is not plausible").format(row.fish_species, row.idx)
+				)
