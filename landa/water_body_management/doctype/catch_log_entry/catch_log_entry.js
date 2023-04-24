9dc263bcba8633cb2024c405535e760534a2c9af
barredterra <14891507+barredterra@users.noreply.github.com>
2023-04-24 13:02:03 +0200

    feat: plausible species, indicator, alert (LAN-81)

--- landa/water_body_management/doctype/catch_log_entry/catch_log_entry.js
+++ landa/water_body_management/doctype/catch_log_entry/catch_log_entry.js
@@ -2,19 +2,24 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Catch Log Entry", {
+	setup: (frm) => {
+		frm.set_indicator_formatter("fish_species", (row) =>
+			row.plausible === 1 ? "green" : "orange"
+		);
+	},
 	onload: (frm) => {
 		if (frm.is_new() && !frm.doc.year) {
 			frm.set_value("year", moment().year() - 1);
 		}
 	},
 	refresh: (frm) => {
-		if (frm.doc.docstatus == 0 && !frm.is_new())	{
+		if (frm.doc.docstatus == 0 && !frm.is_new()) {
 			frm.add_custom_button(__("New Catch Log Entry"), () => {
 				frappe.new_doc(frm.doctype, {
 					organization: frm.doc.organization,
 					year: frm.doc.year,
 				});
 			});
-	   }
-   },
+		}
+	},
 });
--- landa/water_body_management/doctype/catch_log_entry/catch_log_entry.py
+++ landa/water_body_management/doctype/catch_log_entry/catch_log_entry.py
@@ -1,14 +1,14 @@
-# -*- coding: utf-8 -*-
 # Copyright (c) 2021, Real Experts GmbH and contributors
 # For license information, please see license.txt
 
 import frappe
+from frappe import get_doc, get_roles
 from frappe.model.document import Document
 
 
 class CatchLogEntry(Document):
 	def before_insert(self):
-		user_roles = set(frappe.get_roles(frappe.session.user))
+		user_roles = set(get_roles(frappe.session.user))
 		regional_origin = {
 			"LANDA Regional Organization Management",
 			"LANDA Regional Water Body Management",
@@ -22,15 +22,7 @@ class CatchLogEntry(Document):
 			self.origin_of_catch_log_entry = "Verein"
 
 	def validate(self):
-		tolerance = 0.4
+		water_body = get_doc("Water Body", self.water_body)
+		main_species = [row.fish_species for row in water_body.fish_species]
 		for row in self.fish_catches:
-			typical_weight = flt(frappe.db.get_value("Fish Species", row.fish_species, "typical_weight_in_kg"))
-			average_weight = row.weight_in_kg / row.amount
-			if (
-				typical_weight and
-				average_weight < typical_weight * (1 - tolerance) or
-				average_weight > typical_weight * (1 + tolerance)
-			):
-				frappe.msgprint(
-					frappe._("The weight of {0} in row {1} is not plausible").format(row.fish_species, row.idx)
-				)
+			row.plausible = int(row.validate_species(main_species) and row.validate_weight())
