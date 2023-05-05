e57f7aecac5236d6651deefeb5e5467b9628b13a
barredterra <14891507+barredterra@users.noreply.github.com>
2023-05-05 13:28:29 +0200

    fix: missing import (LAN-754)

--- landa/water_body_management/report/catch_log_days_and_counts/catch_log_days_and_counts.py
+++ landa/water_body_management/report/catch_log_days_and_counts/catch_log_days_and_counts.py
@@ -2,9 +2,15 @@
 # For license information, please see license.txt
 
 import frappe
+from frappe import _
 
+from landa.organization_management.doctype.organization.organization import (
+	get_supported_water_bodies,
+)
+from landa.utils import get_current_member_data
 from landa.water_body_management.report.catch_log_statistics.catch_log_statistics import (
-	get_or_filters,
+	REGIONAL_ROLES,
+	STATE_ROLES,
 )
 
 COLUMNS = [
@@ -77,3 +83,35 @@ def get_data(filters):
 
 def execute(filters=None):
 	return COLUMNS, get_data(filters)
+
+
+def get_or_filters():
+	"""Return a dict of filters that restricts the results to what the user is
+	allowed to see.
+	STATE_ROLES		no filters
+	REGIONAL_ROLES	everything related to their water bodys OR to their member organizations
+	LOCAL_ROLES		everything related to their own organization and OR to the water bodys it is supporting
+	"""
+	or_filters = {}
+	user_roles = set(frappe.get_roles())
+
+	if user_roles.intersection(STATE_ROLES):
+		return or_filters
+
+	# User is not a state organization employee
+	member_data = get_current_member_data()
+	if not member_data:
+		frappe.throw(_("You are not a member of any organization."))
+
+	if user_roles.intersection(REGIONAL_ROLES):
+		or_filters["regional_organization"] = member_data.regional_organization
+		or_filters["organization"] = ("like", f"{member_data.regional_organization}-%")
+	else:
+		# User is not in regional organization management
+		or_filters["water_body"] = (
+			"in",
+			get_supported_water_bodies(member_data.local_organization),
+		)
+		or_filters["organization"] = member_data.local_organization
+
+	return or_filters
