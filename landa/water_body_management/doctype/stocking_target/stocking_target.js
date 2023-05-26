7eae618b9db3e7e422613ac2fa0e3438c9bddfc8
diePuppe <tim.puppe@htw-dresden.de>
2023-05-26 18:07:35 +0200

    feat: auto date on stocking_target.js (LAN-58)

--- landa/water_body_management/doctype/stocking_target/stocking_target.js
+++ landa/water_body_management/doctype/stocking_target/stocking_target.js
@@ -3,16 +3,28 @@
 
 {% include "landa/water_body_management/stocking_controller.js" %}
 
+function set_default_year(frm) {
+    // set default year to the current year if it's before november,
+    // else to the next year
+    const now = moment();
+    let year = now.year();
+    if (now.month() >= 11) {
+        year += 1;
+    }
+    frm.set_value("year", year);
+}
+
 frappe.ui.form.on("Stocking Target", {
-	refresh: function (frm) {
-		frm.add_custom_button(__("Create Stocking Measure"), () =>
-			frappe.model.open_mapped_doc({
-				method:
-					"landa.water_body_management.doctype.stocking_target.stocking_target.create_stocking_measure",
-				frm: frm,
-			})
-		);
-	},
+    refresh: function (frm) {
+        set_default_year(frm);
+        frm.add_custom_button(__("Create Stocking Measure"), () =>
+            frappe.model.open_mapped_doc({
+                method:
+                    "landa.water_body_management.doctype.stocking_target.stocking_target.create_stocking_measure",
+                frm: frm,
+            })
+        );
+    },
 });
 
 cur_frm.script_manager.make(landa.StockingController);
