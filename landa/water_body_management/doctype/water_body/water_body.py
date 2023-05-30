512751ea74020e038ed14f5c8806a88aa987dd41
barredterra <14891507+barredterra@users.noreply.github.com>
2023-05-30 19:03:16 +0200

    feat: expire current information (LAN-764)

--- landa/water_body_management/doctype/water_body/water_body.py
+++ landa/water_body_management/doctype/water_body/water_body.py
@@ -5,6 +5,7 @@
 import frappe
 from frappe import _
 from frappe.model.document import Document
+from frappe.utils.data import today
 
 from landa.utils import get_current_member_data
 
@@ -16,3 +17,18 @@ class WaterBody(Document):
 			self.organization != current_member_data.regional_organization
 		):
 			frappe.throw(_("You can only edit a Water Body if it belongs to your regional Organization."))
+
+
+def remove_outdated_information():
+	for name in frappe.get_all(
+		"Water Body",
+		filters=[
+			["current_information_expires_on", "is", "set"],
+			["current_information_expires_on", "<=", today()],
+		],
+		pluck="name",
+	):
+		water_body = frappe.get_doc("Water Body", name)
+		water_body.current_public_information = None
+		water_body.current_information_expires_on = None
+		water_body.save()
