72ec75006dc9a311f05b970e5fff8b1be79e1eb1
diePuppe <tim.puppe@htw-dresden.de>
2023-06-03 09:07:45 +0200

    adding function to utils.js

--- landa/public/js/utils.js
+++ landa/public/js/utils.js
@@ -7,4 +7,14 @@ landa.utils = {
 				landa.queries.regional_organization_query;
 		}
 	},
+	set_default_year: function set_default_year(frm) {
+    // set default year to the current year if it's before november,
+    // else to the next year
+    const now = moment();
+    let year = now.year();
+    if (now.month() >= 11) {
+        year += 1;
+    }
+    frm.set_value("year", year);
+}
 };
--- landa/water_body_management/doctype/stocking_target/stocking_target.js
+++ landa/water_body_management/doctype/stocking_target/stocking_target.js
@@ -2,21 +2,11 @@
 // For license information, please see license.txt
 
 {% include "landa/water_body_management/stocking_controller.js" %}
-
-function set_default_year(frm) {
-    // set default year to the current year if it's before november,
-    // else to the next year
-    const now = moment();
-    let year = now.year();
-    if (now.month() >= 11) {
-        year += 1;
-    }
-    frm.set_value("year", year);
-}
+{% include "landa/public/js/utils.js" %}
 
 frappe.ui.form.on("Stocking Target", {
     refresh: function (frm) {
-        set_default_year(frm);
+        landa.utils.set_default_year(frm);
         frm.add_custom_button(__("Create Stocking Measure"), () =>
             frappe.model.open_mapped_doc({
                 method:
