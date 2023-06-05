5441c5fc418cca37953d5324853a2bea67efbb22
diePuppe <95628567+diePuppe@users.noreply.github.com>
2023-06-05 15:09:39 +0200

    Update landa/public/js/utils.js
    Co-authored-by: Raffael Meyer <14891507+barredterra@users.noreply.github.com>

--- landa/public/js/utils.js
+++ landa/public/js/utils.js
@@ -8,13 +8,13 @@ landa.utils = {
 		}
 	},
 	set_default_year: function set_default_year(frm) {
-    // set default year to the current year if it's before november,
-    // else to the next year
-    	const now = moment();
-    	let year = now.year();
-    	if (now.month() >= 11) {
-        	year += 1;
-    	}
-    	frm.set_value("year", year);
+		// set default year to the current year if it's before november,
+		// else to the next year
+		const now = moment();
+		let year = now.year();
+		if (now.month() >= 11) {
+			year += 1;
+		}
+		frm.set_value("year", year);
 	}
 };
