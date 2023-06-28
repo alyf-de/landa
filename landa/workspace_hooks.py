47b47f6a50936f97fcb26e74f07382063cdb8943
diePuppe <tim.puppe@htw-dresden.de>
2023-06-28 19:48:01 +0200

    feat: LAN-45

--- landa/workspace_hooks.py
+++ landa/workspace_hooks.py
@@ -1,7 +1,8 @@
 import frappe
 
+
 def validate_workspace(doc, method):
-	if doc.doctype == "Workspace":
-		if doc.hide_custom == 0:
-			doc.hide_custom = 1
-			frappe.msgprint("Die Checkbox 'Hide Custom' wurde automatisch auf 1 gesetzt.")
+    if doc.doctype == "Workspace":
+        if doc.hide_custom == 0:
+            doc.hide_custom = 1
+            frappe.msgprint("Die Checkbox 'Hide Custom' wurde automatisch auf 1 gesetzt.")
