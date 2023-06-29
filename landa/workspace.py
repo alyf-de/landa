7bcb79edf26a01f1c86baf2853290fbe3a96bfa6
diePuppe <tim.puppe@htw-dresden.de>
2023-06-29 18:14:46 +0200

    feat: LAN-45

--- landa/workspace.py
+++ landa/workspace.py
@@ -1,10 +1,7 @@
-import frappe
-
-
 def validate_workspace(doc, method):
 	if (
-		doc.for_user and
-		doc.hide_custom == 0 and
-		doc.extends in ("Water Body Management", "Organization Management", "Order Management")
+		doc.for_user
+		and doc.hide_custom == 0
+		and doc.extends in ("Water Body Management", "Organization Management", "Order Management")
 	):
 		doc.hide_custom = 1
