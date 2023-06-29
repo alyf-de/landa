4cfbe344618716ee96d20ad7789b72b118c4ac0d
diePuppe <95628567+diePuppe@users.noreply.github.com>
2023-06-29 17:27:42 +0200

    Update landa/workspace_hooks.py
    Co-authored-by: Raffael Meyer <14891507+barredterra@users.noreply.github.com>

--- landa/workspace_hooks.py
+++ landa/workspace_hooks.py
@@ -2,7 +2,9 @@ import frappe
 
 
 def validate_workspace(doc, method):
-    if doc.doctype == "Workspace":
-        if doc.hide_custom == 0:
-            doc.hide_custom = 1
-            frappe.msgprint("Die Checkbox 'Hide Custom' wurde automatisch auf 1 gesetzt.")
+	if (
+		doc.for_user and
+		doc.hide_custom == 0 and
+		doc.extends in ("Water Body Management", "Organization Management", "Order Management")
+	):
+		doc.hide_custom = 1
