672b61ba686ad1f61d99a7009b929278c7a06aaa
diePuppe <tim.puppe@htw-dresden.de>
2023-07-18 10:26:06 +0200

    feat: LAN-761

--- landa/water_body_management/report/catch_log_days_and_counts/catch_log_days_and_counts.py
+++ landa/water_body_management/report/catch_log_days_and_counts/catch_log_days_and_counts.py
@@ -1,13 +1,13 @@
 # Copyright (c) 2022, Real Experts GmbH and contributors
 # For license information, please see license.txt
 
+from typing import List
+
 import frappe
 from frappe import _
-from typing import List
-from pypika.functions import Coalesce, Sum, Substring
+from pypika.functions import Coalesce, Substring, Sum
 from pypika.queries import Table
 from pypika.terms import Criterion
-
 
 from landa.organization_management.doctype.organization.organization import (
 	get_supported_water_bodies,
@@ -68,17 +68,14 @@ def get_data(filters):
 	by_all_regional_orgs = get_subquery(entry, qb_filters)
 	by_foreign_regional_orgs = get_subquery(
 		entry,
-		qb_filters
-		+ [Substring(entry.organization, 1, 3) != entry.regional_organization],
+		qb_filters + [Substring(entry.organization, 1, 3) != entry.regional_organization],
 		# index starts at 1
 	)
 
 	proportion_foreign = (
 		frappe.qb.from_(by_all_regional_orgs)
 		.left_join(by_foreign_regional_orgs)
-		.on(
-			(by_all_regional_orgs.water_body == by_foreign_regional_orgs.water_body)
-		)
+		.on(by_all_regional_orgs.water_body == by_foreign_regional_orgs.water_body)
 		.select(
 			by_all_regional_orgs.water_body,
 			(
@@ -89,15 +86,10 @@ def get_data(filters):
 		)
 	)
 
-
-
-
 	query = (
 		frappe.qb.from_(entry)
 		.left_join(proportion_foreign)
-		.on(
-			(entry.water_body == proportion_foreign.water_body)
-		)
+		.on(entry.water_body == proportion_foreign.water_body)
 		.select(
 			entry.year,
 			entry.water_body,
@@ -109,12 +101,11 @@ def get_data(filters):
 	if is_regional_or_state_employee():
 		query = query.select(proportion_foreign.by_foreign_regional_org)
 
-	query = filter_and_group(query, entry,qb_filters)
+	query = filter_and_group(query, entry, qb_filters)
 
 	columns = [col["fieldname"] for col in COLUMNS]
 	if is_regional_or_state_employee():
 		columns.append("by_foreign_regional_org")
-
 
 	def tuple_to_dict(tup, columns):
 		return {columns[i]: tup[i] for i in range(len(tup))}
@@ -128,7 +119,10 @@ def get_data(filters):
 	return [postprocess(row) for row in data]
 
 
-def get_qb_filters(filters, entry,):
+def get_qb_filters(
+	filters,
+	entry,
+):
 	filters["workflow_state"] = "Approved"
 	fish_species = filters.pop("fish_species", None)
 	from_year = filters.pop("from_year", None)
@@ -144,14 +138,18 @@ def get_qb_filters(filters, entry,):
 
 	return qb_filters
 
+
 def add_conditions(query, conditions):
 	for condition in conditions:
 		query = query.where(condition)
 
 	return query
 
+
 def get_user_roles():
 	return set(frappe.get_roles())
+
+
 def add_or_filters(query, entry):
 	"""Return a dict of filters that restricts the results to what the user is
 	allowed to see.
@@ -188,22 +186,20 @@ def add_or_filters(query, entry):
 	return query.where(entry.organization.like(f"{member_data.local_organization}%"))
 
 
-
 def filter_and_group(query, entry: Table, qb_filters: List[Criterion]):
 	query = add_conditions(query, qb_filters)
 	query = add_or_filters(query, entry)
 	return query
 
+
 def get_subquery(entry: Table, qb_filters: List[Criterion]):
-	subquery = (
-		frappe.qb.from_(entry)
-		.select(
-			entry.water_body,
-			Sum(entry.fishing_days).as_("total_fishing_days"),
-		)
+	subquery = frappe.qb.from_(entry).select(
+		entry.water_body,
+		Sum(entry.fishing_days).as_("total_fishing_days"),
 	)
 
-	return filter_and_group(subquery, entry,  qb_filters)
+	return filter_and_group(subquery, entry, qb_filters)
+
 
 def is_regional_or_state_employee():
 	user_roles = get_user_roles()
