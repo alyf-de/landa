202aad0f804d7e7407129d3578cde56f80d0d1d6
Raffael Meyer <14891507+barredterra@users.noreply.github.com>
2023-07-18 16:23:05 +0200

    fix: fieldname in condition (LAN-44)

--- landa/water_body_management/doctype/catch_log_entry/catch_log_entry.js
+++ landa/water_body_management/doctype/catch_log_entry/catch_log_entry.js
@@ -25,7 +25,7 @@ frappe.ui.form.on("Catch Log Entry", {
 		const current_year = today.getFullYear();
 		const current_month = today.getMonth();
 
-		if (!frm.doc.year_field) {
+		if (!frm.doc.year) {
 			frm.set_value("year", current_month < 6 ? current_year - 1 : current_year);
 		}
 	},
