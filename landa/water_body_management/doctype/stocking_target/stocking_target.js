9b96e6c0fb6185785752d0d478d11d44ed9e6158
diePuppe <tim.puppe@htw-dresden.de>
2023-08-16 12:49:20 +0200

    feat: set auto date on stocking target(LAN-84)

--- landa/water_body_management/doctype/stocking_target/stocking_target.js
+++ landa/water_body_management/doctype/stocking_target/stocking_target.js
@@ -6,9 +6,6 @@
 
 frappe.ui.form.on("Stocking Target", {
 	refresh: function (frm) {
-		if (frm.is_new() && !frm.doc.year) {
-			landa.utils.set_default_year(frm);
-		}
 		frm.add_custom_button(__("Create Stocking Measure"), () =>
 			frappe.model.open_mapped_doc({
 				method:
@@ -17,6 +14,19 @@ frappe.ui.form.on("Stocking Target", {
 			})
 		);
 	},
-});
+	onload: function (frm) {
+		const today = new Date();
+		const current_year = today.getFullYear();
+
+		let target_year;
+		if (today.getMonth() < 10) {
+			target_year = current_year;
+		}
+		else {
+			target_year = current_year + 1;
+		}
+		frm.set_value('year', target_year);
+	}
+	});
 
 cur_frm.script_manager.make(landa.StockingController);
