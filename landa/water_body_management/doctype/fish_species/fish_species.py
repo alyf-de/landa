8cd6df4aeeb0f26d21a9fed9f49d18d71b012cdd
marination <maricadsouza221197@gmail.com>
2023-08-18 19:35:50 +0530

    feat: Fish Species API
    - Returns cached list of fish species with their data
    - No cache if ID is passed

--- landa/water_body_management/doctype/fish_species/fish_species.py
+++ landa/water_body_management/doctype/fish_species/fish_species.py
@@ -1,9 +1,70 @@
 # Copyright (c) 2021, Real Experts GmbH and contributors
 # For license information, please see license.txt
+from typing import Dict, List
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class FishSpecies(Document):
-	pass
+	def on_update(self):
+		build_fish_species_cache()
+
+	def on_trash(self):
+		build_fish_species_cache()
+
+
+def get_fish_species_data(id: str = None) -> List[Dict]:
+	"""Return fish species from cache."""
+	if id:
+		# We do not cache ID since it's uniqueness makes the API performant
+		return query_fish_species_data(id)
+
+	cache_exists = frappe.cache().hexists("fish_species_data", "all")
+	if not cache_exists:
+		build_fish_species_cache()
+
+	return frappe.cache().hget("fish_species_data", "all")
+
+
+def build_fish_species_cache():
+	"""Rebuild entire fish species data and set in cache."""
+	frappe.cache().hset("fish_species_data", "all", query_fish_species_data())
+
+
+def query_fish_species_data(id: str = None) -> List[Dict]:
+	site_url = frappe.utils.get_url()
+
+	def get_absolute_link(file):
+		# If the image is not a link, prepend the site url to get the absolute link
+		return file if file.startswith(("http://", "https://")) else (site_url + file)
+
+	fish_species = frappe.qb.DocType("Fish Species")
+	query = frappe.qb.from_(fish_species).select(
+		fish_species.title.as_("id"),
+		fish_species.short_code,
+		fish_species.scientific_name,
+		fish_species.close_season,
+		fish_species.minimum_size,
+		fish_species.general_fishing_limit,
+		fish_species.special_fishing_limit,
+		fish_species.traits,
+		fish_species.image,
+	)
+
+	if id:
+		query = query.where(fish_species.title == id)
+
+	result = query.run(as_dict=True)
+
+	if not result:
+		return []
+
+	for row in result:
+		# images must be absolute URLs
+		if not row.image:
+			continue
+
+		row.image = get_absolute_link(row.image)
+
+	return result
