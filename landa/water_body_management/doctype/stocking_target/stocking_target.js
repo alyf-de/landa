a9ab9f57c7c4381786f727c411bb82a75ba54de3
barredterra <14891507+barredterra@users.noreply.github.com>
2023-08-18 18:10:54 +0200

    refactor: set_default_year (LAN-84)

--- landa/water_body_management/doctype/stocking_target/stocking_target.js
+++ landa/water_body_management/doctype/stocking_target/stocking_target.js
@@ -6,9 +6,6 @@
 
 frappe.ui.form.on("Stocking Target", {
 	refresh: function (frm) {
-		if (frm.is_new() && !frm.doc.year) {
-			landa.utils.set_default_year(frm);
-		}
 		frm.add_custom_button(__("Create Stocking Measure"), () =>
 			frappe.model.open_mapped_doc({
 				method:
