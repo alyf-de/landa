bc6a4a86358e63ea9c0839d994aecb1a39110fcd
scdanieli <23150094+scdanieli@users.noreply.github.com>
2023-08-22 00:19:15 +0200

    feat: create stocking targets from measures  (LAN-117)

--- landa/water_body_management/doctype/stocking_measure/stocking_measure.py
+++ landa/water_body_management/doctype/stocking_measure/stocking_measure.py
@@ -19,3 +19,53 @@ class StockingMeasure(StockingController):
 		if stocking_target.status == "Draft":
 			stocking_target.status = "In Progress"
 			stocking_target.save()
+
+
+@frappe.whitelist()
+def create_stocking_targets(stocking_measure_names, year):
+	import json
+
+	if isinstance(stocking_measure_names, str):
+		stocking_measure_names = json.loads(stocking_measure_names)
+
+	stocking_measures = frappe.get_all(
+		"Stocking Measure",
+		filters={"name": ["in", stocking_measure_names]},
+		fields=[
+			"name",
+			"fish_species",
+			"fish_type_for_stocking",
+			"organization",
+			"water_body",
+			"weight",
+			"quantity",
+		],
+	)
+
+	stocking_targets = {}
+
+	for stocking_measure in stocking_measures:
+		primary_key = (
+			stocking_measure["fish_species"],
+			stocking_measure["fish_type_for_stocking"],
+			stocking_measure["water_body"],
+		)
+
+		if primary_key not in stocking_targets:
+			stocking_targets[primary_key] = {
+				"year": int(year),
+				"organization": stocking_measure["organization"],
+				"water_body": stocking_measure["water_body"],
+				"fish_species": stocking_measure["fish_species"],
+				"fish_type_for_stocking": stocking_measure["fish_type_for_stocking"],
+				"weight": 0,
+				"quantity": 0,
+			}
+
+		stocking_targets[primary_key]["weight"] += stocking_measure["weight"]
+		stocking_targets[primary_key]["quantity"] += stocking_measure["quantity"]
+
+	for stocking_target in list(stocking_targets.values()):
+		doc = frappe.new_doc("Stocking Target")
+		doc.update(stocking_target)
+		doc.save()
