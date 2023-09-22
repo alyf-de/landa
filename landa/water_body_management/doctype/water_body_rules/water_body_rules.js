ff34e2001e3f6db78657d19bf1229c119327146f
barredterra <14891507+barredterra@users.noreply.github.com>
2023-09-22 15:23:58 +0200

    feat: toggle HTML (LAN-779)

--- landa/water_body_management/doctype/water_body_rules/water_body_rules.js
+++ landa/water_body_management/doctype/water_body_rules/water_body_rules.js
@@ -1,8 +1,28 @@
 // Copyright (c) 2023, ALYF GmbH and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Water Body Rules', {
-	// refresh: function(frm) {
+frappe.ui.form.on("Water Body Rules", {
+	refresh: function (frm) {
+		frm.add_custom_button(__("Toggle HTML"), function () {
+			frm.trigger("toggle_html");
+		});
+	},
+	toggle_html: function (frm) {
+		for (let fieldname of ["water_body_rules", "privacy_policy", "imprint"]) {
+			let df = frm.fields_dict[fieldname].df;
 
-	// }
+			// overwrites Code formatter defined in apps/frappe/frappe/public/js/frappe/form/formatters.js
+			df.formatter = df.read_only ? undefined : format_html;
+			frm.set_df_property(fieldname, "fieldtype", df.read_only ? "HTML Editor" : "Code");
+			frm.set_df_property(fieldname, "options", df.read_only ? "" : "HTML");
+			frm.set_df_property(fieldname, "read_only", !df.read_only);
+		}
+	}
 });
+
+// modifies Code formatter defined in apps/frappe/frappe/public/js/frappe/form/formatters.js
+// to pretty print HTML
+function format_html(value, df) {
+	const _value = df.options == "HTML" ? html_beautify(value) : value;
+	return "<pre>" + (_value == null ? "" : $("<div>").text(_value).html()) + "</pre>";
+}
