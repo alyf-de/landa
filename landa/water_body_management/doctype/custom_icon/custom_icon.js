14112aed2afa84230dd40306617586f8bf84baa8
barredterra <14891507+barredterra@users.noreply.github.com>
2023-11-10 13:03:48 +0100

    feat: remind user to attach an image file

--- landa/water_body_management/doctype/custom_icon/custom_icon.js
+++ landa/water_body_management/doctype/custom_icon/custom_icon.js
@@ -1,8 +1,10 @@
 // Copyright (c) 2023, ALYF GmbH and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Custom Icon', {
-	// refresh: function(frm) {
-
-	// }
+frappe.ui.form.on("Custom Icon", {
+	refresh: function (frm) {
+		if (!frm.is_new() && !frm.doc.icon) {
+			frm.dashboard.set_headline(__("Please attach an icon file."));
+		}
+	},
 });
