adca96b6170bd0882720488f747d0be3da02a533
barredterra <14891507+barredterra@users.noreply.github.com>
2023-11-23 15:04:16 +0100

    fix: convert multi to line strings (LAN-794)

---
+++ landa/patches/multi_to_line.py
@@ -0,0 +1,43 @@
+import json
+
+import frappe
+
+
+def execute():
+	"""
+	Water Body: convert all MultiLineString to LineString in the geo shapes.
+
+	Leaflet Draw doesn't support editing the MultiLineString (imported from the
+	former GIS), so we need to convert them to LineString. Also, we drop
+	obsolete properties from the GeoJSON data.
+	"""
+	for wb_name in frappe.get_all(
+		"Water Body", filters={"location": ("like", "%MultiLineString%")}, pluck="name"
+	):
+		geojson_str = frappe.db.get_value("Water Body", wb_name, "location")
+		geojson = json.loads(geojson_str)
+
+		if "features" not in geojson:
+			continue
+
+		features = []
+		modified = False
+		for feature in geojson["features"]:
+			if feature["geometry"]["type"] != "MultiLineString":
+				features.append(feature)
+			else:
+				features.extend(
+					{
+						"type": "Feature",
+						"geometry": {"type": "LineString", "coordinates": line},
+						"properties": {
+							"osm_id": feature["properties"].get("osm_id"),
+						},
+					}
+					for line in feature["geometry"]["coordinates"]
+				)
+				modified = True
+
+		if modified:
+			geojson["features"] = features
+			frappe.db.set_value("Water Body", wb_name, "location", json.dumps(geojson))
