c1c48487913c88b2ff537e656c9929e7a0fcd80a
marination <maricadsouza221197@gmail.com>
2023-12-29 21:16:19 +0100

    feat: Water Body Management Settings
    - Store firebase credentials

---
+++ landa/water_body_management/doctype/water_body_management_settings/test_water_body_management_settings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, ALYF GmbH and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+
+class TestWaterBodyManagementSettings(unittest.TestCase):
+	pass
---
+++ landa/water_body_management/doctype/water_body_management_settings/water_body_management_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, ALYF GmbH and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Water Body Management Settings', {
+	// refresh: function(frm) {
+
+	// }
+});
