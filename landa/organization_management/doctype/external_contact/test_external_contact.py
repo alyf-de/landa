5282a6a13786adb97ce2c52088e77652d8c8cc58
barredterra <14891507+barredterra@users.noreply.github.com>
2024-03-01 17:30:33 +0100

    test(External Contact): add unit test

--- landa/organization_management/doctype/external_contact/test_external_contact.py
+++ landa/organization_management/doctype/external_contact/test_external_contact.py
@@ -1,9 +1,15 @@
 # Copyright (c) 2022, Real Experts GmbH and Contributors
 # See license.txt
 
-# import frappe
-import unittest
+import frappe
+from frappe.tests.utils import FrappeTestCase
 
 
-class TestExternalContact(unittest.TestCase):
-	pass
+class TestExternalContact(FrappeTestCase):
+	def test_autoname(self):
+		contacts = frappe.get_all("External Contact", pluck="name")
+		self.assertIn("EXT-REG-0001", contacts)
+		self.assertIn("EXT-REG-0002", contacts)
+
+	def test_full_name(self):
+		self.assertTrue(frappe.db.exists("External Contact", {"full_name": "Jane Doe"}))
