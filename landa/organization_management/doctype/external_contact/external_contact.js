a0ad27ed276193804d7208bfd0f7a68aa4f81677
barredterra <14891507+barredterra@users.noreply.github.com>
2024-03-01 16:26:12 +0100

    feat(External Contact): link to regional organization (LAN-802)
    (cherry picked from commit 331c34b495e9daaf61d745c9cbfe049e4fc4b1c7)
    
    # Conflicts:
    #	landa/patches.txt

--- landa/organization_management/doctype/external_contact/external_contact.js
+++ landa/organization_management/doctype/external_contact/external_contact.js
@@ -1,7 +1,16 @@
-// Copyright (c) 2022, Real Experts GmbH and contributors
+// Copyright (c) 2024, ALYF GmbH and contributors
 // For license information, please see license.txt
 
 frappe.ui.form.on('External Contact', {
+    setup: function(frm) {
+        frm.set_query('organization', function() {
+            return {
+                filters: {
+                    parent_organization: 'LV',
+                }
+            };
+        });
+    },
     refresh: function(frm) {
         // Automatically add the backlink to External Contact when a new Address or
         // Contact is added.
@@ -13,6 +22,10 @@ frappe.ui.form.on('External Contact', {
 
         if (frm.is_new()) {
             frappe.contacts.clear_address_and_contact(frm);
+
+            if (!frm.doc.organization && frappe.boot.landa?.regional_organization) {
+                frm.set_value('organization', frappe.boot.landa.regional_organization);
+            }
         }
         else {
             frappe.contacts.render_address_and_contact(frm);
