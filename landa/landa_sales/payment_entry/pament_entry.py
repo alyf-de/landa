e0cb821bfc6c2188fdd3c42c014aac817336b280
barredterra <14891507+barredterra@users.noreply.github.com>
2024-03-14 18:32:36 +0100

    fix(Payment Entry): handle organization link in backend (LAN-799)

--- landa/landa_sales/payment_entry/pament_entry.py
+++ landa/landa_sales/payment_entry/pament_entry.py
@@ -1,8 +1,18 @@
 import frappe
+from frappe import _
+
+# We need to set and validate the organization in the backend because the field
+# is read-only and does not always get filled automatically (e.g. when creating
+# a Payment Entry from a Sales Invoice).
 
 
 def before_validate(payment_entry, event):
 	set_organization(payment_entry)
+
+
+def validate(payment_entry, event):
+	if not payment_entry.organization:
+		frappe.throw(_("Payment Entry must be linked to an organization."))
 
 
 def set_organization(payment_entry):
