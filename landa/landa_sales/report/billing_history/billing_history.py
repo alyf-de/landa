47a202173e3e475ca65f6e2bbf285511f7faa7b2
barredterra <14891507+barredterra@users.noreply.github.com>
2024-03-14 19:32:13 +0100

    feat: new report Billing History (LAN-815)

---
+++ landa/landa_sales/report/billing_history/billing_history.py
@@ -0,0 +1,106 @@
+# Copyright (c) 2024, ALYF GmbH and contributors
+# For license information, please see license.txt
+
+from typing import Optional
+
+import frappe
+from frappe import _
+
+
+def execute(filters=None):
+	organization = filters.pop("organization", None)
+	year_of_settlement = filters.pop("year_of_settlement", None)
+
+	if not isinstance(organization, str):
+		frappe.throw(_("Invalid organization"))
+
+	if year_of_settlement and not isinstance(year_of_settlement, int):
+		frappe.throw(_("Invalid year of settlement"))
+
+	return get_columns(), get_data(organization, year_of_settlement)
+
+
+def get_columns():
+	return [
+		{
+			"fieldname": "posting_date",
+			"fieldtype": "Date",
+			"label": "Posting Date",
+			"width": 200,
+		},
+		{
+			"fieldname": "voucher_type",
+			"label": "Voucher Type",
+			"fieldtype": "Link",
+			"options": "DocType",
+			"width": 200,
+			"hidden": 1,
+		},
+		{
+			"fieldname": "voucher_no",
+			"label": "Voucher No",
+			"fieldtype": "Dynamic Link",
+			"options": "voucher_type",
+			"width": 200,
+		},
+		{
+			"fieldname": "grand_total",
+			"fieldtype": "Currency",
+			"label": "Grand Total",
+			"width": 200,
+		},
+	]
+
+
+def get_data(organization: Optional[str], year_of_settlement: Optional[int] = None):
+	filters = {
+		"docstatus": 1,
+	}
+
+	if organization:
+		filters["organization"] = organization
+
+	if year_of_settlement:
+		filters["year_of_settlement"] = year_of_settlement
+
+	data = []
+	for posting_date, grand_total, voucher_name in frappe.get_list(
+		"Sales Invoice",
+		fields=[
+			"posting_date",
+			"base_grand_total",
+			"name",
+		],
+		filters=filters,
+		as_list=True,
+	):
+		data.append(
+			(
+				posting_date,
+				"Sales Invoice",
+				voucher_name,
+				grand_total,
+			)
+		)
+
+	for posting_date, grand_total, voucher_name, payment_type in frappe.get_list(
+		"Payment Entry",
+		fields=[
+			"posting_date",
+			"base_paid_amount",
+			"name",
+			"payment_type",
+		],
+		filters=filters,
+		as_list=True,
+	):
+		data.append(
+			(
+				posting_date,
+				"Payment Entry",
+				voucher_name,
+				grand_total * -1 if payment_type == "Receive" else grand_total,
+			)
+		)
+
+	return sorted(data, key=lambda row: row[0])
