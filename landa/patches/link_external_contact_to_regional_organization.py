8da88af4fa73302c8becfd4f757ff24fc11a7e52
barredterra <14891507+barredterra@users.noreply.github.com>
2024-03-15 19:21:26 +0100

    patch: auto commit on many writes

--- landa/patches/link_external_contact_to_regional_organization.py
+++ landa/patches/link_external_contact_to_regional_organization.py
@@ -2,6 +2,8 @@ import frappe
 from frappe.model.naming import make_autoname
 from frappe.model.rename_doc import rename_doc
 
+from landa.utils import autocommit
+
 
 def execute():
 	"""Adjust the existing External Contacts to link to the regional organization
@@ -9,26 +11,27 @@ def execute():
 
 	Also change the name of the External Contacts to reflect the new organization.
 	"""
-	for external_contact, organization in frappe.get_all(
-		"External Contact",
-		filters={"organization": ("is", "set")},
-		fields=["name", "organization"],
-		as_list=True,
-	):
-		regional_organization = frappe.get_doc("Organization", organization[:3])
-		ext_contact = frappe.get_doc("External Contact", external_contact)
-		ext_contact.db_set(
-			{
-				"organization": regional_organization.name,
-				"organization_name": regional_organization.organization_name,
-			},
-			update_modified=False,
-		)
-
-		rename_doc(
+	with autocommit():
+		for external_contact, organization in frappe.get_all(
 			"External Contact",
-			external_contact,
-			make_autoname(f"EXT-{regional_organization.name}-.####", "External Contact"),
-			force=True,
-			show_alert=False,
-		)
+			filters={"organization": ("is", "set")},
+			fields=["name", "organization"],
+			as_list=True,
+		):
+			regional_organization = frappe.get_doc("Organization", organization[:3])
+			ext_contact = frappe.get_doc("External Contact", external_contact)
+			ext_contact.db_set(
+				{
+					"organization": regional_organization.name,
+					"organization_name": regional_organization.organization_name,
+				},
+				update_modified=False,
+			)
+
+			rename_doc(
+				"External Contact",
+				external_contact,
+				make_autoname(f"EXT-{regional_organization.name}-.####", "External Contact"),
+				force=True,
+				show_alert=False,
+			)
