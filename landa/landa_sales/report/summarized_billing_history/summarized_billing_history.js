70c7a317ce33c250b517e6fd1c34f6ba6cc356ea
barredterra <14891507+barredterra@users.noreply.github.com>
2024-03-15 16:19:15 +0100

    feat: new report "Summarized Billing History" (LAN-826)
    Replacement for "Landa Deliveries And Payments Summaries", based on invoices instead of deliveries

--- landa/landa_sales/report/summarized_billing_history/summarized_billing_history.js
+++ landa/landa_sales/report/summarized_billing_history/summarized_billing_history.js
@@ -1,16 +1,14 @@
-// Copyright (c) 2023, ALYF GmbH and contributors
+// Copyright (c) 2024, ALYF GmbH and contributors
 // For license information, please see license.txt
 /* eslint-disable */
 
-frappe.query_reports["LANDA Deliveries and Payments Summaries"] = {
+frappe.query_reports["Summarized Billing History"] = {
 	filters: [
 		{
 			fieldname: "organization",
 			fieldtype: "Link",
 			label: "Organization",
-			mandatory: 0,
 			options: "Organization",
-			wildcard_filter: 0,
 			default: frappe.boot.landa.organization,
 			get_query: function () {
 				return {
@@ -22,10 +20,8 @@ frappe.query_reports["LANDA Deliveries and Payments Summaries"] = {
 		},
 		{
 			fieldname: "year_of_settlement",
-			fieldtype: "Data",
+			fieldtype: "Int",
 			label: "Year of Settlement",
-			mandatory: 0,
-			wildcard_filter: 0,
 			default: new Date().getFullYear(),
 		},
 	],
