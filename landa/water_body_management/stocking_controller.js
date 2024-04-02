d825720d91477bc72d92c9548478871a3a6e8def
barredterra <14891507+barredterra@users.noreply.github.com>
2024-04-02 14:26:03 +0200

    fix(Stocking): remove code related to price

--- landa/water_body_management/stocking_controller.js
+++ landa/water_body_management/stocking_controller.js
@@ -1,7 +1,7 @@
 frappe.provide("landa.StockingController");
 
 landa.StockingController = frappe.ui.form.Controller.extend({
-    onload() {
+	onload() {
 		this.frm.set_query("organization", function (doc) {
 			return {
 				filters: {
@@ -28,7 +28,7 @@ landa.StockingController = frappe.ui.form.Controller.extend({
 		this.weight();
 		this.quantity();
 	},
-    weight() {
+	weight() {
 		if (!this.frm.doc.weight || !this.frm.doc.water_body_size) {
 			this.frm.set_value("weight_per_water_body_size", 0.0);
 		} else {
@@ -41,8 +41,6 @@ landa.StockingController = frappe.ui.form.Controller.extend({
 				`Kg / ${this.frm.doc.water_body_size_unit}`
 			);
 		}
-
-		this.update_price();
 	},
 	quantity() {
 		if (!this.frm.doc.quantity || !this.frm.doc.water_body_size) {
@@ -55,19 +53,6 @@ landa.StockingController = frappe.ui.form.Controller.extend({
 			this.frm.set_value(
 				"unit_of_quantity_per_water_body_size",
 				`Stk / ${this.frm.doc.water_body_size_unit}`
-			);
-		}
-	},
-	price_per_kilogram() {
-		this.update_price();
-	},
-	update_price() {
-		if (!this.frm.doc.weight || !this.frm.doc.price_per_kilogram) {
-			this.frm.set_value("price_for_total_weight", 0);
-		} else {
-			this.frm.set_value(
-				"price_for_total_weight",
-				flt(this.frm.doc.weight * this.frm.doc.price_per_kilogram, 2)
 			);
 		}
 	},
