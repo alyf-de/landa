43275d21de8f5f1d8ab26051e8694a81c340ffd3
barredterra <14891507+barredterra@users.noreply.github.com>
2024-05-27 01:53:45 +0200

    style(Member Count): format filter definition

--- landa/landa_stock/report/landa_member_count/landa_member_count.js
+++ landa/landa_stock/report/landa_member_count/landa_member_count.js
@@ -3,47 +3,49 @@
 /* eslint-disable */
 
 frappe.query_reports["LANDA Member Count"] = {
-	"filters": [
+	filters: [
 		{
-			"fieldname": "organization",
-			"fieldtype": "Link",
-			"label": __("Organization"),
-			"mandatory": 1,
-			"options": "Organization",
-			"wildcard_filter": 0,
-			"default": frappe.defaults.get_user_default("Organization"),
-			"get_query": function() {
+			fieldname: "organization",
+			fieldtype: "Link",
+			label: __("Organization"),
+			mandatory: 1,
+			options: "Organization",
+			wildcard_filter: 0,
+			default: frappe.defaults.get_user_default("Organization"),
+			get_query: function () {
 				return {
 					filters: [
-						["Organization", "parent_organization", "in", ["LV", "AVL", "AVS", "AVE"]]
-					]
-				}
-			}
+						["Organization", "parent_organization", "in", ["LV", "AVL", "AVS", "AVE"]],
+					],
+				};
+			},
 		},
 		{
-			"fieldname": "year",
-			"fieldtype": "Data",
-			"label": __("Year"),
-			"wildcard_filter": 0,
+			fieldname: "year",
+			fieldtype: "Data",
+			label: __("Year"),
+			wildcard_filter: 0,
 		},
 		{
-			"fieldname": "fishing_area",
-			"fieldtype": "Link",
-			"label": __("Fishing Area"),
-			"options": "Fishing Area",
-			"get_query": function() {
+			fieldname: "fishing_area",
+			fieldtype: "Link",
+			label: __("Fishing Area"),
+			options: "Fishing Area",
+			get_query: function () {
 				return {
 					filters: {
-						"organization": frappe.query_report.get_filter_value("organization").substring(0, 3)
-					}
-				}
-			}
+						organization: frappe.query_report
+							.get_filter_value("organization")
+							.substring(0, 3),
+					},
+				};
+			},
 		},
 		{
-			"fieldname": "total",
-			"fieldtype": "Check",
-			"label": __("Total (only for regional org.)"),
-			"wildcard_filter": 0,
-		}
-	]
+			fieldname: "total",
+			fieldtype: "Check",
+			label: __("Total (only for regional org.)"),
+			wildcard_filter: 0,
+		},
+	],
 };
