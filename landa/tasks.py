7db74c334879dc0987e5dc8d41ac403f4ae32f76
barredterra <14891507+barredterra@users.noreply.github.com>
2024-05-27 22:28:44 +0200

    fix: enqueue daily tasks so they run independently (LAN-845)

--- landa/tasks.py
+++ landa/tasks.py
@@ -1,20 +1,22 @@
-def daily():
-	from landa.organization_management.doctype.member_function.member_function import (
-		disable_expired_member_functions,
-	)
-	from landa.water_body_management.doctype.lease_contract.lease_contract import (
-		disable_expired_lease_contracts,
-	)
-	from landa.water_body_management.doctype.water_body.water_body import remove_outdated_information
+from frappe import enqueue
 
-	disable_expired_member_functions()
-	disable_expired_lease_contracts()
-	remove_outdated_information()
+
+def daily():
+	enqueue(
+		"landa.organization_management.doctype.member_function.member_function.disable_expired_member_functions",
+		queue="long",
+	)
+	enqueue(
+		"landa.water_body_management.doctype.lease_contract.lease_contract.disable_expired_lease_contracts",
+		queue="long",
+	)
+	enqueue(
+		"landa.water_body_management.doctype.water_body.water_body.remove_outdated_information",
+		queue="long",
+	)
 
 
 def all():
-	from frappe import enqueue
-
 	# probably no longer needed, disabled in hooks
 	enqueue(
 		"landa.organization_management.address.address.rename_addresses",
