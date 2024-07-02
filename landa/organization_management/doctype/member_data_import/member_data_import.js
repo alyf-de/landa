3c00942f6c5d0f3ea7ef71fdc5c72874b1bc2e0b
barredterra <14891507+barredterra@users.noreply.github.com>
2024-07-02 17:00:12 +0200

    fix: only show alert if member is available

--- landa/organization_management/doctype/member_data_import/member_data_import.js
+++ landa/organization_management/doctype/member_data_import/member_data_import.js
@@ -3,12 +3,14 @@
 
 frappe.ui.form.on("Member Data Import", {
 	after_save: function (frm) {
-		frappe.show_alert({
-			message: __("LANDA Member {0} has been created or updated.", [
-				frappe.utils.get_form_link("LANDA Member", frm.doc.member, true),
-			]),
-			indicator: "green",
-		});
-		frappe.new_doc("Member Data Import");
+		if (frm.doc.member) {
+			frappe.show_alert({
+				message: __("LANDA Member {0} has been created or updated.", [
+					frappe.utils.get_form_link("LANDA Member", frm.doc.member, true),
+				]),
+				indicator: "green",
+			});
+			frappe.new_doc("Member Data Import");
+		}
 	},
 });
