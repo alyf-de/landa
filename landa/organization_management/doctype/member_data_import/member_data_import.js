fc70f30e755967b7430079bff9e25823f4c3cb0f
barredterra <14891507+barredterra@users.noreply.github.com>
2024-07-02 16:15:22 +0200

    feat: show success and create new member data import

--- landa/organization_management/doctype/member_data_import/member_data_import.js
+++ landa/organization_management/doctype/member_data_import/member_data_import.js
@@ -1,8 +1,14 @@
 // Copyright (c) 2021, Real Experts GmbH and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Member Data Import', {
-	// refresh: function(frm) {
-
-	// }
+frappe.ui.form.on("Member Data Import", {
+	after_save: function (frm) {
+		frappe.show_alert({
+			message: __("Member {0} has been created.", [
+				frappe.utils.get_form_link("LANDA Member", frm.doc.member, true),
+			]),
+			indicator: "green",
+		});
+		frappe.new_doc("Member Data Import");
+	},
 });
