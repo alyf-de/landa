ed97dc77258adc94f1632286d8df9bc8a43cffdf
barredterra <14891507+barredterra@users.noreply.github.com>
2024-07-02 16:19:20 +0200

    fix: add translation

--- landa/organization_management/doctype/member_data_import/member_data_import.js
+++ landa/organization_management/doctype/member_data_import/member_data_import.js
@@ -4,7 +4,7 @@
 frappe.ui.form.on("Member Data Import", {
 	after_save: function (frm) {
 		frappe.show_alert({
-			message: __("Member {0} has been created.", [
+			message: __("LANDA Member {0} has been created or updated.", [
 				frappe.utils.get_form_link("LANDA Member", frm.doc.member, true),
 			]),
 			indicator: "green",
