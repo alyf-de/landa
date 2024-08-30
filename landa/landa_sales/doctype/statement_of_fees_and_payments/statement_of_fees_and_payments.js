09efe4a68a254d99df362ad0c2ac47ab4b0a07a1
Raffael Meyer <14891507+barredterra@users.noreply.github.com>
2024-08-30 21:27:17 +0200

    feat: Beitragsabrechnung (LAN-844) (#267)

---
+++ landa/landa_sales/doctype/statement_of_fees_and_payments/statement_of_fees_and_payments.js
@@ -0,0 +1,65 @@
+// Copyright (c) 2024, ALYF GmbH and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on("Statement of Fees and Payments", {
+	setup(frm) {
+		frm.set_query("customer_address", (doc) => {
+			return {
+				filters: [
+					["Dynamic Link", "link_doctype", "=", "Customer"],
+					["Dynamic Link", "link_name", "=", doc.customer],
+				],
+			};
+		});
+
+		frm.set_query("company_address", (doc) => {
+			return {
+				filters: [
+					["Dynamic Link", "link_doctype", "=", "Company"],
+					["Dynamic Link", "link_name", "=", doc.company],
+				],
+			};
+		});
+
+		frm.set_query("payment_entry", "payments", (doc) => {
+			return {
+				filters: {
+					company: doc.company,
+					year_of_settlement: doc.year_of_settlement,
+					party_type: "Customer",
+					party: doc.customer,
+					docstatus: 1,
+				},
+			};
+		});
+	},
+
+	refresh(frm) {
+		if (frm.is_new() && !frm.doc.year_of_settlement) {
+			frm.set_value("year_of_settlement", new Date().getFullYear() - 1);
+		}
+	},
+
+	async company(frm) {
+		if (frm.doc.company) {
+			const default_address = await frappe.xcall(
+				"erpnext.setup.doctype.company.company.get_default_company_address",
+				{ name: frm.doc.company, existing_address: frm.doc.company_address || "" }
+			);
+
+			if (default_address) {
+				frm.set_value("company_address", default_address);
+			} else {
+				frm.set_value("company_address", "");
+			}
+		}
+	},
+
+	customer_address(frm) {
+		erpnext.utils.get_address_display(frm, "customer_address", "customer_address_display");
+	},
+
+	company_address(frm) {
+		erpnext.utils.get_address_display(frm, "company_address", "company_address_display");
+	},
+});
---
+++ landa/landa_sales/doctype/statement_of_fees_and_payments/statement_of_fees_and_payments.py
@@ -0,0 +1,79 @@
+# Copyright (c) 2024, ALYF GmbH and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.docstatus import DocStatus
+from frappe.model.document import Document
+from pandas import DataFrame as df
+
+
+class StatementofFeesandPayments(Document):
+	def before_save(self):
+		self.fetch_payments()
+		self.fetch_sales()
+		self.calculate_totals()
+
+	def fetch_payments(self):
+		self.payments = []
+		for pe in frappe.get_list(
+			"Payment Entry",
+			filters={
+				"company": self.company,
+				"party_type": "Customer",
+				"party": self.customer,
+				"docstatus": DocStatus.submitted(),
+				"year_of_settlement": self.year_of_settlement,
+			},
+			fields=[
+				"name",
+				"reference_date",
+				"payment_type",
+				"base_paid_amount",
+			],
+		):
+			self.append(
+				"payments",
+				{
+					"payment_entry": pe.name,
+					"payment_type": pe.payment_type,
+					"reference_date": pe.reference_date,
+					"amount": pe.base_paid_amount if pe.payment_type == "Receive" else -pe.base_paid_amount,
+				},
+			)
+
+	def fetch_sales(self):
+		invoice_rows = frappe.get_list(
+			"Sales Invoice",
+			filters={
+				"company": self.company,
+				"customer": self.customer,
+				"docstatus": DocStatus.submitted(),
+				"year_of_settlement": self.year_of_settlement,
+			},
+			fields=[
+				"`tabSales Invoice Item`.item_code as item_code",
+				"`tabSales Invoice Item`.item_name as item_name",
+				"`tabSales Invoice Item`.qty as qty",
+				"`tabSales Invoice Item`.base_amount as amount",
+			],
+		)
+
+		if not invoice_rows:
+			return
+
+		rows_df = df.from_records(invoice_rows)
+
+		rows_df["billed"] = rows_df["qty"].clip(lower=0)
+		rows_df["credited"] = -1 * rows_df["qty"].clip(upper=0)
+		rows_df["net_billed"] = rows_df["billed"] - rows_df["credited"]
+		rows_df["rate"] = rows_df["amount"] / rows_df["net_billed"]
+
+		rows_df.drop(columns="qty", inplace=True)
+		rows_df = rows_df.groupby(["item_code", "item_name"]).sum().reset_index()
+
+		self.sales = []
+		self.extend("sales", rows_df.to_dict(orient="records"))
+
+	def calculate_totals(self):
+		self.sum_of_payments = sum(p.amount for p in self.payments)
+		self.sum_of_sales = sum(s.amount for s in self.sales)
