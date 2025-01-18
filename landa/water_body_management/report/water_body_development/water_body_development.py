51b56ec34f052a5fad603ddf035eb1aa1d4d4b8e
barredterra <14891507+barredterra@users.noreply.github.com>
2025-01-18 15:31:55 +0100

    fix: apply permissions and remove repetitive values

--- landa/water_body_management/report/water_body_development/water_body_development.py
+++ landa/water_body_management/report/water_body_development/water_body_development.py
@@ -1,9 +1,17 @@
 # Copyright (c) 2025, ALYF GmbH and contributors
 # For license information, please see license.txt
 
+from typing import TYPE_CHECKING
+
 import frappe
 from frappe import _
 from pypika.functions import Sum
+
+from landa.utils import get_current_member_data
+
+if TYPE_CHECKING:
+	from pypika.queries import QueryBuilder
+	from pypika.terms import Field
 
 
 def execute(filters=None):
@@ -125,6 +133,53 @@ def get_data(from_year, to_year):
 			fishing_days_query.fishing_days,
 		)
 		.where((catch_query.catch_quantity > 0) | (stocking_measure_query.stocking_quantity > 0))
+		.orderby(water_body.name, fish_species.name)
 	)
 
-	return query.run()
+	query = apply_permissions(query, regional_org_field=water_body.organization)
+	results = query.run()
+
+	if not results:
+		return []
+
+	return remove_repetitive_values(
+		[list(result) for result in results],
+		column_to_check=0,
+		columns_to_clear=[0, 1, -1],
+	)
+
+
+def apply_permissions(query: "QueryBuilder", regional_org_field: "Field") -> "QueryBuilder":
+	"""Ensure that the user only sees data for their own regional organization.
+
+	Args:
+	    query: query to apply permissions to.
+	    regional_org_field: field to check for member's regional organization.
+	"""
+	member_data = get_current_member_data()
+	if member_data.regional_organization:
+		query = query.where(regional_org_field == member_data.regional_organization)
+
+	return query
+
+
+def remove_repetitive_values(
+	results: list[list], column_to_check: int, columns_to_clear: list[int]
+) -> list[list]:
+	"""Remove repetitive values in a list of lists.
+
+	Args:
+	    results: list of lists, where each list represents a row.
+	    column_to_check: index of the column to check for duplicates.
+	    columns_to_clear: list of indices of the columns to be set to None.
+	"""
+	previous_value = None
+	for result in results:
+		current_value = result[column_to_check]
+		if current_value == previous_value:
+			for column_index in columns_to_clear:
+				result[column_index] = None
+
+		previous_value = current_value
+
+	return results
