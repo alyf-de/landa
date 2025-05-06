edfabf9c476d6a8f6dd6570dfc0dcb8cf5c9f240
barredterra <14891507+barredterra@users.noreply.github.com>
2025-05-06 14:21:27 +0200

    refactor: extract `validate_year_of_settlement`

--- landa/landa_sales/utils.py
+++ landa/landa_sales/utils.py
@@ -1,5 +1,10 @@
+from typing import TYPE_CHECKING
+
 import frappe
 from frappe import _
+
+if TYPE_CHECKING:
+	from erpnext.controllers.selling_controller import SellingController
 
 
 def validate_company_price_list(company: str, price_list: str):
@@ -18,3 +23,22 @@ def validate_company_customer(company: str, customer: str):
 	customer_organization = frappe.db.get_value("Customer", customer, "organization")
 	if company_abbr not in customer_organization:
 		frappe.throw(_("Customer {0} is not valid for Company {1}.").format(customer, company))
+
+
+def validate_year_of_settlement(doc: "SellingController"):
+	if not doc.year_of_settlement:
+		return
+
+	for item in doc.items:
+		from_year, to_year = frappe.db.get_value(
+			"Item", item.item_code, ["valid_from_year", "valid_to_year"]
+		)
+
+		if (from_year and doc.year_of_settlement < from_year) or (
+			to_year and doc.year_of_settlement > to_year
+		):
+			frappe.throw(
+				_("Row {0}: Item {1} is not valid for year of settlement {2}.").format(
+					item.idx, frappe.bold(item.item_name), frappe.bold(doc.year_of_settlement)
+				)
+			)
