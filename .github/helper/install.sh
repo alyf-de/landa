5e91f30d52c82b1f6bc7d0affd62a2c27c92c12e
barredterra <14891507+barredterra@users.noreply.github.com>
2025-11-29 19:55:56 +0100

    ci: update to v15

--- .github/helper/install.sh
+++ .github/helper/install.sh
@@ -8,7 +8,7 @@ sudo apt update && sudo apt install redis-server libcups2-dev
 
 pip install frappe-bench
 
-bench init --skip-assets --frappe-branch version-14 --python "$(which python)" frappe-bench
+bench init --skip-assets --frappe-branch version-15 --python "$(which python)" frappe-bench
 
 mysql --host 127.0.0.1 --port 3306 -u root -proot -e "SET GLOBAL character_set_server = 'utf8mb4'"
 mysql --host 127.0.0.1 --port 3306 -u root -proot -e "SET GLOBAL collation_server = 'utf8mb4_unicode_ci'"
@@ -26,8 +26,8 @@ sed -i 's/schedule:/# schedule:/g' Procfile
 sed -i 's/socketio:/# socketio:/g' Procfile
 sed -i 's/redis_socketio:/# redis_socketio:/g' Procfile
 
-bench get-app erpnext --branch version-14
-bench get-app https://github.com/alyf-de/eu_einvoice --branch version-14
+bench get-app erpnext --branch version-15
+bench get-app https://github.com/alyf-de/eu_einvoice --branch version-15
 bench get-app landa "${GITHUB_WORKSPACE}"
 
 bench start &> bench_run_logs.txt &
