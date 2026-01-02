f267d179dd7c7c066400cf8f4e143785936e1d26
Marc-Constantin Enke <marc@alyf.de>
2026-01-02 16:01:46 +0100

    refactor: update install.py to new setup

--- landa/install.py
+++ landa/install.py
@@ -3,14 +3,22 @@ import os
 import frappe
 from frappe import get_hooks
 from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
+from frappe.custom.doctype.customize_form.customize_form import (
+	docfield_properties,
+	doctype_properties,
+)
 from frappe.custom.doctype.property_setter.property_setter import make_property_setter
 
 import landa
 
+from .custom_fields import get_custom_fields
+from .property_setters import get_property_setters
+
 
 def after_install():
+	make_custom_fields()
+	make_property_setters()
 	complete_setup_wizard_for_test()
-
 	update_system_settings()
 	make_custom_fields()
 	make_property_setters()
@@ -100,17 +108,35 @@ def update_system_settings():
 
 
 def make_custom_fields():
-	create_custom_fields(frappe.get_hooks("landa_custom_fields", {}))
+	create_custom_fields(get_custom_fields())
 
 
 def make_property_setters():
-	for doctypes, property_setters in frappe.get_hooks("landa_property_setters", {}).items():
+	for doctypes, property_setters in get_property_setters().items():
 		if isinstance(doctypes, str):
 			doctypes = (doctypes,)
 
 		for doctype in doctypes:
 			for property_setter in property_setters:
-				make_property_setter(doctype, *property_setter, for_doctype=not property_setter[0])
+				if property_setter[0]:
+					for_doctype = False
+					property_type = docfield_properties.get(
+						property_setter[1], "Data"
+					)  # Data fallback for field_order
+				else:
+					for_doctype = True
+					property_type = doctype_properties.get(
+						property_setter[1], "Data"
+					)  # Data fallback for field_order
+
+				make_property_setter(
+					doctype=doctype,
+					fieldname=property_setter[0],
+					property=property_setter[1],
+					value=property_setter[2],
+					property_type=property_type,
+					for_doctype=for_doctype,
+				)
 
 
 def update_stock_settings():
