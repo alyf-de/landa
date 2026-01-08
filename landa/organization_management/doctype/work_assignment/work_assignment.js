57c06f8261fc9c77ff42050316c214546d8b73f7
Marc-Constantin Enke <marc@alyf.de>
2026-01-08 16:54:36 +0100

    feat: set organization based on user role

--- landa/organization_management/doctype/work_assignment/work_assignment.js
+++ landa/organization_management/doctype/work_assignment/work_assignment.js
@@ -3,12 +3,21 @@
 
 frappe.ui.form.on("Work Assignment", {
 	setup(frm) {
+		const can_select_any =
+			frappe.user.has_role("LANDA State Organization Employee") ||
+			frappe.user.has_role("LANDA Regional Organization Management");
+		const organization = frappe.boot.landa?.organization;
+
+		if (organization) {
+			frm.set_value("organization", organization);
+		}
+
 		frm.set_query("organization", function () {
-			return {
-				filters: {
-					is_group: 0,
-				},
-			};
+			let filters = { is_group: 0 };
+			if (!can_select_any && organization) {
+				filters.name = organization;
+			}
+			return { filters };
 		});
 
 		frm.set_query("member", "members", function (doc) {
@@ -18,5 +27,14 @@ frappe.ui.form.on("Work Assignment", {
 				},
 			};
 		});
+	},
+});
+
+frappe.ui.form.on("Work Assignment Member", {
+	member(frm, cdt, cdn) {
+		let row = frappe.get_doc(cdt, cdn);
+		if (!row.duration && frm.doc.planned_duration) {
+			frappe.model.set_value(cdt, cdn, "duration", frm.doc.planned_duration);
+		}
 	},
 });
