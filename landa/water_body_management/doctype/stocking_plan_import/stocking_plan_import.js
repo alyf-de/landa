faec18bf26a76b80b2aea7118d268b1d3fb1e5f8
Marc-Constantin Enke <marc@alyf.de>
2026-01-10 13:08:20 +0100

    feat: add query filters

--- landa/water_body_management/doctype/stocking_plan_import/stocking_plan_import.js
+++ landa/water_body_management/doctype/stocking_plan_import/stocking_plan_import.js
@@ -1,8 +1,34 @@
 // Copyright (c) 2026, ALYF GmbH and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Stocking Plan Import", {
-// 	refresh(frm) {
+frappe.ui.form.on("Stocking Plan Import", {
+	onload(frm) {
+		frm.set_query("organization", function () {
+			return {
+				filters: {
+					parent_organization: frappe.boot.landa.state_organization,
+					name: frappe.boot.landa.regional_organization,
+				},
+			};
+		});
 
-// 	},
-// });
+		frm.set_query("water_body", function (doc) {
+			return {
+				filters: {
+					organization: doc.organization,
+				},
+			};
+		});
+
+		if (!frm.doc.organization && frappe.boot.landa.regional_organization) {
+			frm.set_value("organization", frappe.boot.landa.regional_organization);
+		}
+
+		landa.utils.set_default_year(frm);
+	},
+
+	after_save(frm) {
+		frm.clear_table("items");
+		frm.refresh_field("items");
+	},
+});
