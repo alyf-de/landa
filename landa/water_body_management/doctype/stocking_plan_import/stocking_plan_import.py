787fe67eed9755c5d45fd98b184e3add8333ced9
Marc-Constantin Enke <marc@alyf.de>
2026-01-10 13:11:49 +0100

    feat: create stocking measures from stocking plan import

--- landa/water_body_management/doctype/stocking_plan_import/stocking_plan_import.py
+++ landa/water_body_management/doctype/stocking_plan_import/stocking_plan_import.py
@@ -1,7 +1,8 @@
 # Copyright (c) 2026, ALYF GmbH and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
 
 
@@ -14,15 +15,49 @@ class StockingPlanImport(Document):
 	if TYPE_CHECKING:
 		from frappe.types import DF
 
+		from landa.water_body_management.doctype.stocking_plan_import_item.stocking_plan_import_item import (
+			StockingPlanImportItem,
+		)
+
+		date: DF.Date
+		items: DF.Table[StockingPlanImportItem]
+		organization: DF.Link
+		water_body: DF.Link
+		year: DF.Int
+
 	# end: auto-generated types
 
 	def db_insert(self, *args, **kwargs):
-		pass
+		for item in self.items:
+			frappe.get_doc(
+				{
+					"doctype": "Stocking Measure",
+					"organization": self.organization,
+					"water_body": self.water_body,
+					"year": self.year,
+					"date": self.date,
+					"fish_species": item.fish_species,
+					"fish_type_for_stocking": item.fish_type_for_stocking,
+					"weight": item.weight,
+					"quantity": item.quantity or 0,
+					"supplier": item.supplier,
+					"status": "In Progress",
+				}
+			).insert()
+
+		frappe.msgprint(
+			_("{0} Stocking Measure(s) created").format(len(self.items)),
+			alert=True,
+			indicator="green",
+		)
 
 	def load_from_db(self):
 		pass
 
 	def db_update(self):
+		pass
+
+	def delete(self):
 		pass
 
 	@staticmethod
