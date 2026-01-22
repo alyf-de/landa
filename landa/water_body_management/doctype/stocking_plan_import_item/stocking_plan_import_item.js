0fd76e15d0a57b5f4da6b1e8dbfb95234ab04760
Marc-Constantin Enke <marc@alyf.de>
2026-01-22 17:48:03 +0100

    refactor: remove false stocking plan import item files

--- landa/water_body_management/doctype/stocking_plan_import_item/stocking_plan_import_item.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2026, ALYF GmbH and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Stocking Plan Import Item", {
-// 	refresh(frm) {
-
-// 	},
-// });
