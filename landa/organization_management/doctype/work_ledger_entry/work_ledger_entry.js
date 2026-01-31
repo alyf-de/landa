9c6798b59aa341f8dce6051968e7591df7663bc7
Marc-Constantin Enke <marc@alyf.de>
2026-01-31 10:51:23 +0100

    feat(Work Ledger Entry): filter members of organization

--- landa/organization_management/doctype/work_ledger_entry/work_ledger_entry.js
+++ landa/organization_management/doctype/work_ledger_entry/work_ledger_entry.js
@@ -1,8 +1,12 @@
 // Copyright (c) 2026, ALYF GmbH and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Work Ledger Entry", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Work Ledger Entry", {
+	setup(frm) {
+		frm.set_query("member", function () {
+			return {
+				filters: { organization: frm.doc.organization },
+			};
+		});
+	},
+});
