7e44751e34c79472ac3bdf38cc6f2cda3c66582c
barredterra <14891507+barredterra@users.noreply.github.com>
2026-02-11 18:52:03 +0100

    refactor: safer cleanup via ORM
    - exclude cancelled records, they don't bother us
    - cancel duplicate submitted records
    - delete duplicate draft records (can be restored from deleted documents)
    - Use autocommit context manager to handle large number of writes

--- landa/patches/delete_duplicate_permits.py
+++ landa/patches/delete_duplicate_permits.py
@@ -1,6 +1,8 @@
 import frappe
 from frappe.utils import update_progress_bar
 
+from landa.utils import autocommit
+
 
 def execute():
 	duplicates = frappe.db.sql("""
@@ -12,7 +14,7 @@ def execute():
 			number,
 			COUNT(*) AS occurrence_count
 		FROM `tabYearly Fishing Permit`
-		WHERE member IS NOT NULL AND member != ''
+		WHERE member IS NOT NULL AND member != '' AND docstatus != 2
 		GROUP BY member, year, type, docstatus, number
 		HAVING COUNT(*) > 1
 		ORDER BY year DESC;
@@ -24,7 +26,7 @@ def execute():
 	for i, (member, year, permit_type, docstatus, number, _count) in enumerate(duplicates):
 		update_progress_bar("Deleting duplicate permits", i, total)
 
-		keep = frappe.db.get_value(
+		record_to_keep = frappe.db.get_value(
 			"Yearly Fishing Permit",
 			filters={
 				"member": member,
@@ -36,14 +38,27 @@ def execute():
 			order_by="creation ASC",
 		)
 
-		frappe.db.delete(
+		records_to_delete = frappe.get_all(
 			"Yearly Fishing Permit",
-			{
-				"name": ("!=", keep),
+			filters={
+				"name": ("!=", record_to_keep),
 				"member": member,
 				"year": year,
 				"type": permit_type,
 				"docstatus": docstatus,
 				"number": number,
 			},
+			fields=["name", "docstatus"],
 		)
+
+		with autocommit():
+			for record in records_to_delete:
+				if record.docstatus == 1:
+					frappe.get_doc("Yearly Fishing Permit", record.name).cancel()
+				else:
+					frappe.delete_doc(
+						"Yearly Fishing Permit",
+						record.name,
+						ignore_permissions=True,
+						ignore_missing=True,
+					)
