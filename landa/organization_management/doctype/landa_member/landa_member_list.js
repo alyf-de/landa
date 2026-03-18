926214ed37b81f69551ade39f063292009d4c238
Raffael Meyer <14891507+barredterra@users.noreply.github.com>
2026-03-18 23:06:33 +0100

    fix: handle duplicate entry error during bulk creation of yearly fishing permits (LAN-894) (#315)
    (cherry picked from commit dccefeae8cc01ec90d3b80d1ba50cdf4b318e7e3)
    
    # Conflicts:
    #	landa/organization_management/doctype/landa_member/landa_member_list.js
    #	landa/translations/de.csv

--- landa/organization_management/doctype/landa_member/landa_member_list.js
+++ landa/organization_management/doctype/landa_member/landa_member_list.js
@@ -108,11 +108,18 @@ frappe.listview_settings["LANDA Member"] = {
 									members: list_view.get_checked_items(true),
 								},
 							)
-							.then((total_created) => {
+							.then((result) => {
+								const total_created = result.num_created;
+								const total_skipped = result.num_skipped;
 								frappe.show_alert({
 									message: __(
+<<<<<<< HEAD
 										"Yearly Fishing Permits have been created for {0} members.",
 										[total_created],
+=======
+										"Permits have been created for {0} members and skipped for {1} members.",
+										[total_created, total_skipped]
+>>>>>>> dccefea (fix: handle duplicate entry error during bulk creation of yearly fishing permits (LAN-894) (#315))
 									),
 									indicator: "green",
 								});
