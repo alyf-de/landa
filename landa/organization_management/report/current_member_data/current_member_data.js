230bb2d41c384ef2421a2aa6209602f3ae5d303e
barredterra <14891507+barredterra@users.noreply.github.com>
2026-03-24 13:34:04 +0100

    fix: HTML opening tags

--- landa/organization_management/report/current_member_data/current_member_data.js
+++ landa/organization_management/report/current_member_data/current_member_data.js
@@ -16,8 +16,10 @@ frappe.query_reports["Current Member Data"] = {
 	onload: function (query_report) {
 		const b = cur_page.page.page.wrapper.find(".sub-heading");
 		b.html(
-			`
-            Sehen Sie sich bitte unbedingt vor der ersten Benutzung <a href="/how-to/Mitgliedsdatenimport" target=_blank style="color: blue;">diese Anleitung</a> an.</p>
+			`<div>
+                <p>
+                    Sehen Sie sich bitte unbedingt vor der ersten Benutzung <a href="/how-to/Mitgliedsdatenimport" target=_blank style="color: blue;">diese Anleitung</a> an.
+                </p>
         </div>`,
 		).toggleClass("hide", false);
 	},
