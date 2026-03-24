c3adf47d9963b308b8d7ad4db2a10abe40596fce
Raffael Meyer <14891507+barredterra@users.noreply.github.com>
2026-03-24 13:05:13 +0100

    perf(Current Member Data): make organization filter mandatory, filter yearly fishing permits (LAN-895) (#318)
    (cherry picked from commit 1f8e062aba3fa08cb042355410ea3b3e7bf16548)
    
    # Conflicts:
    #	landa/organization_management/report/current_member_data/current_member_data.js
    #	landa/organization_management/report/current_member_data/current_member_data.py

--- landa/organization_management/report/current_member_data/current_member_data.js
+++ landa/organization_management/report/current_member_data/current_member_data.js
@@ -3,6 +3,7 @@
 /* eslint-disable */
 
 frappe.query_reports["Current Member Data"] = {
+<<<<<<< HEAD
     "filters": [
         {
             "fieldname": "organization",
@@ -17,6 +18,22 @@ frappe.query_reports["Current Member Data"] = {
     onload: function (query_report) {
         const b = cur_page.page.page.wrapper.find(".sub-heading");
         b.html(`
+=======
+	filters: [
+		{
+			fieldname: "organization",
+			fieldtype: "Link",
+			options: "Organization",
+			label: __("Organization"),
+			mandatory: 1,
+			default: frappe.defaults.get_user_default("Organization"),
+		},
+	],
+	onload: function (query_report) {
+		const b = cur_page.page.page.wrapper.find(".sub-heading");
+		b.html(
+			`
+>>>>>>> 1f8e062 (perf(Current Member Data): make organization filter mandatory, filter yearly fishing permits (LAN-895) (#318))
             Sehen Sie sich bitte unbedingt vor der ersten Benutzung <a href="/how-to/Mitgliedsdatenimport" target=_blank style="color: blue;">diese Anleitung</a> an.</p>
         </div>`).toggleClass("hide", false);
     }
--- landa/organization_management/report/current_member_data/current_member_data.py
+++ landa/organization_management/report/current_member_data/current_member_data.py
@@ -1,19 +1,21 @@
 # Copyright (c) 2013, Real Experts GmbH and contributors
 # For license information, please see license.txt
 
+from datetime import datetime
+
 import frappe
 import pandas as pd
 from frappe import _
 
 
-class LANDACurrentMemberData:
-	def __init__(self, filters):
-		# set attribute to load only members and remove it from filters
-		self.filter = filters
+def execute(filters=None):
+	organization = filters.pop("organization", None)
+	if not organization:
+		frappe.throw(_("Organization is required"))
 
-	def run(self):
-		return self.get_columns(), self.get_data()
+	return get_columns(), get_data(organization)
 
+<<<<<<< HEAD
 	def get_data(self):
 		def frappe_tuple_to_pandas_df(frappe_tuple, fields):
 			# convert to pandas dataframe
@@ -21,25 +23,26 @@ class LANDACurrentMemberData:
 			# set by member ID as dataframe index
 			df.set_index("member", inplace=True)
 			return df
+=======
+>>>>>>> 1f8e062 (perf(Current Member Data): make organization filter mandatory, filter yearly fishing permits (LAN-895) (#318))
 
-		def remove_duplicate_indices(df, index="member", sort_by=None, keep="last"):
-			"""Remove rows in dataframe with duplicate indeces.
-			If sort_by is specified the dataframe is firsted sorted by these columns keeping the entry specified in keep, e.g. 'last'"""
-			if sort_by is not None:
-				df = df.sort_values(sort_by)
-			return df.reset_index().drop_duplicates(subset=[index], keep=keep).set_index(index)
+def get_data(organization: str):
+	"""Assemble rows for the current-member report from several DocTypes.
 
-		def get_link_filters(frappe_tuple):
-			member_ids = [m[0] for m in frappe_tuple]  # list of member names (ID)
-			link_filters = [
-				["Dynamic Link", "link_doctype", "=", "LANDA Member"],
-				["Dynamic Link", "link_name", "in", member_ids],
-			]
-			return member_ids, link_filters
+	Loads LANDA Member master data (respecting the organization filter), Yearly
+	Fishing Permit rows keyed by member (keeping one row per member after
+	sorting by year), and Address rows dynamically linked to those members.
+	The frames are merged and ordered to match `get_columns()`, then
+	returned as tuples with empty strings instead of missing values.
 
-		# define the member master data that are supposed to be loaded
-		member_fields = [
-			"name",
+	Permissions: every fetch goes through Frappe `get_list`, so the result only
+	contains documents and fields the current user is allowed to read.
+	"""
+	members = frappe.get_list(
+		"LANDA Member",
+		filters={"organization": organization},
+		fields=[
+			"name as member",
 			"last_name",
 			"first_name",
 			"date_of_birth",
@@ -55,6 +58,7 @@ class LANDACurrentMemberData:
 			"has_special_yearly_fishing_permit_5",
 			"has_special_yearly_fishing_permit_6",
 			"has_special_yearly_fishing_permit_7",
+<<<<<<< HEAD
 		]
 		members = frappe.db.get_list(
 			"LANDA Member", filters=self.filter, fields=member_fields, as_list=True
@@ -91,11 +95,56 @@ class LANDACurrentMemberData:
 		addresses_df = frappe_tuple_to_pandas_df(addresses, address_fields + ["member"])
 		# rename index column
 		addresses_df.rename({"name": "address_name"}, axis=1, inplace=True)
+=======
+		],
+	)
+	if not members:
+		return ()
 
-		# merge members and addresses from different doctypes
-		data = pd.concat([member_df, fishing_permits_df], axis=1).reindex(member_df.index)
-		data = pd.merge(data, addresses_df, on="member", how="outer")
+	member_df = pd.DataFrame.from_records(members, index="member")
+	this_year = datetime.now().year
+	fishing_permits = frappe.get_list(
+		"Yearly Fishing Permit",
+		filters={
+			"organization": organization,
+			"docstatus": 1,
+			"year": ["in", [this_year - 1, this_year, this_year + 1]],
+		},
+		fields=[
+			"name as yearly_fishing_permit",
+			"member",
+			"year",
+			"type",
+		],
+	)
+	fishing_permits_df = pd.DataFrame.from_records(fishing_permits, index="member")
+	# Remove rows in dataframe with duplicate indeces.
+	# The dataframe is firsted sorted by year keeping the 'last' entry.
+	fishing_permits_df = fishing_permits_df.sort_values(["year"])
+	fishing_permits_df = (
+		fishing_permits_df.reset_index().drop_duplicates(subset=["member"], keep="last").set_index("member")
+	)
 
+	# load addresses from db
+	addresses = frappe.get_list(
+		"Address",
+		filters=[
+			["Dynamic Link", "link_doctype", "=", "LANDA Member"],
+			["Dynamic Link", "link_name", "in", [m.member for m in members]],
+		],
+		fields=[
+			"name as address_name",
+			"address_line1",
+			"pincode",
+			"city",
+			"`tabDynamic Link`.link_name as member",
+		],
+	)
+>>>>>>> 1f8e062 (perf(Current Member Data): make organization filter mandatory, filter yearly fishing permits (LAN-895) (#318))
+
+	addresses_df = pd.DataFrame.from_records(addresses, index="member")
+
+<<<<<<< HEAD
 		# sort dataframe like report columns
 		sorted_columns = [c["fieldname"] for c in self.get_columns()][1:]
 		data = data[sorted_columns]
@@ -105,131 +154,143 @@ class LANDACurrentMemberData:
 		data.reset_index(inplace=True)
 		data = tuple(data.itertuples(index=False, name=None))
 		return data
+=======
+	# merge members and addresses from different doctypes
+	data = pd.concat([member_df, fishing_permits_df], axis=1).reindex(member_df.index)
+	data = pd.merge(data, addresses_df, on="member", how="outer")
+>>>>>>> 1f8e062 (perf(Current Member Data): make organization filter mandatory, filter yearly fishing permits (LAN-895) (#318))
 
-	def get_columns(self):
-		return [
-			{
-				"label": _("Member ID"),
-				"fieldtype": "Link",
-				"fieldname": "name",
-				"options": "LANDA Member",
-			},
-			{
-				"label": _("Last Name"),
-				"fieldtype": "Data",
-				"fieldname": "last_name",
-			},
-			{
-				"label": _("First Name"),
-				"fieldtype": "Data",
-				"fieldname": "first_name",
-			},
-			{
-				"label": _("Date of Birth"),
-				"fieldtype": "Date",
-				"fieldname": "date_of_birth",
-			},
-			{
-				"label": _("Address ID"),
-				"fieldtype": "Link",
-				"fieldname": "address_name",
-				"options": "Address",
-			},
-			{
-				"label": _("Address Line 1"),
-				"fieldtype": "Data",
-				"fieldname": "address_line1",
-			},
-			{
-				"label": _("Pincode"),
-				"fieldtype": "Data",
-				"fieldname": "pincode",
-			},
-			{
-				"label": _("City"),
-				"fieldtype": "Data",
-				"fieldname": "city",
-			},
-			{
-				"label": _("Is Supporting Member"),
-				"fieldtype": "Check",
-				"fieldname": "is_supporting_member",
-			},
-			{
-				"label": _("Has Key"),
-				"fieldtype": "Check",
-				"fieldname": "has_key",
-			},
-			{
-				"label": _("Youth Membership"),
-				"fieldtype": "Check",
-				"fieldname": "youth_membership",
-			},
-			{
-				"label": _("Additional Information"),
-				"fieldtype": "Data",
-				"fieldname": "additional_information",
-			},
-			{
-				"label": _("Organization"),
-				"fieldtype": "Link",
-				"fieldname": "organization",
-				"options": "Organization",
-			},
-			{
-				"label": _("ID Yearly Fishing Permit"),
-				"fieldtype": "Link",
-				"fieldname": "yearly_fishing_permit",
-				"options": "Yearly Fishing Permit",
-			},
-			{
-				"label": _("Year of Yearly Fishing Permit"),
-				"fieldtype": "Data",
-				"fieldname": "year",
-			},
-			{
-				"label": _("Yearly Fishing Permit Type"),
-				"fieldtype": "Link",
-				"fieldname": "type",
-				"options": "Yearly Fishing Permit Type",
-			},
-			{
-				"label": _("Hat Sachsen-Anhalt Erlaubnisschein"),
-				"fieldtype": "Check",
-				"fieldname": "has_special_yearly_fishing_permit_1",
-			},
-			{
-				"label": _("Hat Brandenburg Erlaubnisschein"),
-				"fieldtype": "Check",
-				"fieldname": "has_special_yearly_fishing_permit_2",
-			},
-			{
-				"label": _("Hat Berlin Erlaubnisschein"),
-				"fieldtype": "Check",
-				"fieldname": "has_special_yearly_fishing_permit_3",
-			},
-			{
-				"label": _("Hat Mecklenburg-Vorpommern Erlaubnisschein"),
-				"fieldtype": "Check",
-				"fieldname": "has_special_yearly_fishing_permit_4",
-			},
-			{
-				"label": _("Hat Saalekaskade Erlaubnisschein"),
-				"fieldtype": "Check",
-				"fieldname": "has_special_yearly_fishing_permit_5",
-			},
-			{
-				"label": _("Hat LAVT Erlaubnisschein"),
-				"fieldtype": "Check",
-				"fieldname": "has_special_yearly_fishing_permit_6",
-			},
-			{
-				"label": _("Hat VANT Erlaubnisschein"),
-				"fieldtype": "Check",
-				"fieldname": "has_special_yearly_fishing_permit_7",
-			},
-		]
+	# sort dataframe like report columns
+	sorted_columns = [c["fieldname"] for c in get_columns()][1:]
+	data = data[sorted_columns]
+	# replace NaNs with empty strings
+	data = data.fillna("")
+	# convert data back to tuple
+	data = data.reset_index()
+	data = tuple(data.itertuples(index=False, name=None))
+	return data
 
 
-def execute(filters=None):
-	return LANDACurrentMemberData(filters).run()
+def get_columns():
+	return [
+		{
+			"label": _("Member ID"),
+			"fieldtype": "Link",
+			"fieldname": "name",
+			"options": "LANDA Member",
+		},
+		{
+			"label": _("Last Name"),
+			"fieldtype": "Data",
+			"fieldname": "last_name",
+		},
+		{
+			"label": _("First Name"),
+			"fieldtype": "Data",
+			"fieldname": "first_name",
+		},
+		{
+			"label": _("Date of Birth"),
+			"fieldtype": "Date",
+			"fieldname": "date_of_birth",
+		},
+		{
+			"label": _("Address ID"),
+			"fieldtype": "Link",
+			"fieldname": "address_name",
+			"options": "Address",
+		},
+		{
+			"label": _("Address Line 1"),
+			"fieldtype": "Data",
+			"fieldname": "address_line1",
+		},
+		{
+			"label": _("Pincode"),
+			"fieldtype": "Data",
+			"fieldname": "pincode",
+		},
+		{
+			"label": _("City"),
+			"fieldtype": "Data",
+			"fieldname": "city",
+		},
+		{
+			"label": _("Is Supporting Member"),
+			"fieldtype": "Check",
+			"fieldname": "is_supporting_member",
+		},
+		{
+			"label": _("Has Key"),
+			"fieldtype": "Check",
+			"fieldname": "has_key",
+		},
+		{
+			"label": _("Youth Membership"),
+			"fieldtype": "Check",
+			"fieldname": "youth_membership",
+		},
+		{
+			"label": _("Additional Information"),
+			"fieldtype": "Data",
+			"fieldname": "additional_information",
+		},
+		{
+			"label": _("Organization"),
+			"fieldtype": "Link",
+			"fieldname": "organization",
+			"options": "Organization",
+		},
+		{
+			"label": _("ID Yearly Fishing Permit"),
+			"fieldtype": "Link",
+			"fieldname": "yearly_fishing_permit",
+			"options": "Yearly Fishing Permit",
+		},
+		{
+			"label": _("Year of Yearly Fishing Permit"),
+			"fieldtype": "Data",
+			"fieldname": "year",
+		},
+		{
+			"label": _("Yearly Fishing Permit Type"),
+			"fieldtype": "Link",
+			"fieldname": "type",
+			"options": "Yearly Fishing Permit Type",
+		},
+		{
+			"label": _("Hat Sachsen-Anhalt Erlaubnisschein"),
+			"fieldtype": "Check",
+			"fieldname": "has_special_yearly_fishing_permit_1",
+		},
+		{
+			"label": _("Hat Brandenburg Erlaubnisschein"),
+			"fieldtype": "Check",
+			"fieldname": "has_special_yearly_fishing_permit_2",
+		},
+		{
+			"label": _("Hat Berlin Erlaubnisschein"),
+			"fieldtype": "Check",
+			"fieldname": "has_special_yearly_fishing_permit_3",
+		},
+		{
+			"label": _("Hat Mecklenburg-Vorpommern Erlaubnisschein"),
+			"fieldtype": "Check",
+			"fieldname": "has_special_yearly_fishing_permit_4",
+		},
+		{
+			"label": _("Hat Saalekaskade Erlaubnisschein"),
+			"fieldtype": "Check",
+			"fieldname": "has_special_yearly_fishing_permit_5",
+		},
+		{
+			"label": _("Hat LAVT Erlaubnisschein"),
+			"fieldtype": "Check",
+			"fieldname": "has_special_yearly_fishing_permit_6",
+		},
+		{
+			"label": _("Hat VANT Erlaubnisschein"),
+			"fieldtype": "Check",
+			"fieldname": "has_special_yearly_fishing_permit_7",
+		},
+	]
