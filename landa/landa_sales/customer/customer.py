e0b8707e2457e55affc22dd5041639abf2f0a656
Marc-Constantin Enke <marc@alyf.de>
2026-04-24 10:07:11 +0200

    refactor(Customer): move to variables for translations

--- landa/landa_sales/customer/customer.py
+++ landa/landa_sales/customer/customer.py
@@ -1,6 +1,5 @@
 import frappe
 from frappe import _
-from frappe.utils import format
 
 
 @frappe.whitelist()
@@ -8,16 +7,28 @@ def get_member_functions_html(organization: str) -> str:
 	member_functions = frappe.get_list(
 		"Member Function",
 		filters={"organization": organization, "status": "Active"},
-		fields=["name", "member_function_category", "member", "member_first_name", "member_last_name", "start_date"],
+		fields=[
+			"name",
+			"member_function_category",
+			"member",
+			"member_first_name",
+			"member_last_name",
+			"start_date",
+		],
 		order_by="member_function_category asc",
 	)
 
 	if not member_functions:
-		return f"<p>{_('No active member functions.')}</p>"
+		empty_message = _("No active member functions.")
+		return f"<p>{empty_message}</p>"
+
+	function_label = _("Function")
+	member_label = _("Member")
+	since_label = _("Since")
 
 	rows = ""
 	for mf in member_functions:
-		start_date = format(mf.start_date, {"fieldtype": "Date"})
+		start_date = frappe.format(mf.start_date, {"fieldtype": "Date"})
 		rows += f"""<tr>
 			<td><a href="/app/member-function/{mf.name}">{mf.member_function_category}</a></td>
 			<td><a href="/app/landa-member/{mf.member}">{mf.member_first_name} {mf.member_last_name}</a></td>
@@ -28,9 +39,9 @@ def get_member_functions_html(organization: str) -> str:
 	<table class="table table-bordered table-sm" style="margin-bottom: 20px">
 		<thead>
 			<tr>
-				<th>{_("Function")}</th>
-				<th>{_("Member")}</th>
-				<th>{_("Since")}</th>
+				<th>{function_label}</th>
+				<th>{member_label}</th>
+				<th>{since_label}</th>
 			</tr>
 		</thead>
 		<tbody>
