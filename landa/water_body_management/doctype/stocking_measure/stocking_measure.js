47708b0af5a026be411c77e8f1c1b87acb46b1a4
Raffael Meyer <14891507+barredterra@users.noreply.github.com>
2026-08-02 17:56:46 +0200

    feat(Stocking Site): new table with fish type, swapped for old one (#343)

--- landa/water_body_management/doctype/stocking_measure/stocking_measure.js
+++ landa/water_body_management/doctype/stocking_measure/stocking_measure.js
@@ -9,12 +9,14 @@ cur_frm.script_manager.make(landa.water_body_management.StockingMeasure);
 
 frappe.ui.form.on("Stocking Measure", {
 	setup(frm) {
-		frm.set_query("stocking_site", (doc, cdt, cdn) => {
+		frm.set_query("stocking_site", (doc) => {
 			return {
-				filters: [
-					["Stocking Site", "water_body", "=", doc.water_body],
-					["Fish Species Table", "fish_species", "=", doc.fish_species],
-				]
+				query: "landa.water_body_management.doctype.stocking_measure.stocking_measure.stocking_site_query",
+				filters: {
+					water_body: doc.water_body,
+					fish_species: doc.fish_species,
+					fish_type_for_stocking: doc.fish_type_for_stocking,
+				},
 			};
 		});
 	},
